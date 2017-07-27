d458dad1f567d8c4c60cda63e156659ff494a504
Pablo <pablo.aledo@gmail.com>
2017-07-27 09:45:47 +0100

    bash install

--- remote_install.sh
+++ remote_install.sh
@@ -9,7 +9,7 @@ echo 'pkg install git'   >> .pkg
 echo 'pkg install unzip' >> .pkg
 echo 'git clone https://github.com/pablo-aledo/dotfiles.git .dotfiles' >> .pkg
 echo 'cd .dotfiles'      >> .pkg
-echo './install.sh'      >> .pkg
+echo 'bash ./install.sh' >> .pkg
 echo 'touch .paths'      >> .pkg
 
 bash .pkg
