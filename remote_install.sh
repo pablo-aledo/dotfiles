3646f165e5d633a95de3aa8c955bc8776ee0a70d
Pablo <pablo.aledo@gmail.com>
2017-08-09 11:57:02 +0100

    fixing creation path for .paths

--- remote_install.sh
+++ remote_install.sh
@@ -10,6 +10,6 @@ echo 'pkg install unzip' >> .pkg
 echo 'git clone https://github.com/pablo-aledo/dotfiles.git .dotfiles' >> .pkg
 echo 'cd .dotfiles'      >> .pkg
 echo 'bash ./install.sh' >> .pkg
-echo 'touch .paths'      >> .pkg
+echo 'touch ~/.paths'      >> .pkg
 
 bash .pkg
