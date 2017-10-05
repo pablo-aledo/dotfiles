9f718b997ffaa64ec23789e59cf7bad5f59ad779
Pablo <pablo.aledo@gmail.com>
2017-10-05 09:52:05 +0100

    small changes in novnc2

--- pkg/Mint_17/novnc2/install.sh
+++ pkg/Mint_17/novnc2/install.sh
@@ -1,8 +1,8 @@
 mkdir ~/.vnc 
-cp passwd ~/.vnc/
-cp xstartup ~/.vnc/
+//cp passwd ~/.vnc/
+//cp xstartup ~/.vnc/
+//chmod +x ~/.vnc/xstartup
 sudo apt-get install -y vnc4server
-vncserver
 
 cd
 git clone https://github.com/novnc/noVNC.git
