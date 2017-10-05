a8238a26042062f0208f518ddea488c315f14e0a
Pablo <pablo.aledo@gmail.com>
2017-10-05 11:09:32 +0100

    Fixing bugs in the installation of novnc2

--- pkg/Mint_17/novnc2/install.sh
+++ pkg/Mint_17/novnc2/install.sh
@@ -1,8 +1,11 @@
 mkdir ~/.vnc 
-//cp passwd ~/.vnc/
-//cp xstartup ~/.vnc/
-//chmod +x ~/.vnc/xstartup
+#cp passwd ~/.vnc/
+#cp xstartup ~/.vnc/
+#chmod +x ~/.vnc/xstartup
 sudo apt-get install -y vnc4server
+
+gsettings set org.gnome.desktop.wm.keybindings  panel-main-menu '[]'
+gsettings set org.gnome.desktop.wm.keybindings  show-desktop '[]'
 
 cd
 git clone https://github.com/novnc/noVNC.git
