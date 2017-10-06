8631c64d6ee3f5060ebc27b3fc06ab891459f4fa
Pablo <pablo.aledo@gmail.com>
2017-10-06 17:00:13 +0100

    updating gotty installer

--- pkg/Mint_17/gotty/install.sh
+++ pkg/Mint_17/gotty/install.sh
@@ -1,4 +1,6 @@
 cd 
-wget https://github.com/yudai/gotty/releases/download/pre-release/gotty_linux_amd64.tar.gz
-tar -xvzf gotty_linux_amd64.tar.gz
+#wget https://github.com/yudai/gotty/releases/download/pre-release/gotty_linux_amd64.tar.gz
+wget https://github.com/yudai/gotty/releases/download/pre-release/gotty_2.0.0-alpha.2_linux_amd64.tar.gz
+#wget https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz
+tar -xvzf gotty_*.tar.gz
 sudo mv ./gotty /bin
