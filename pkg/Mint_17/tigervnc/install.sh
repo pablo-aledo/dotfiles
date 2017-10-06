46a1fe7be6d7baefa905769d116a0b242b419967
Pablo <pablo.aledo@gmail.com>
2017-10-06 09:56:57 +0100

    tigervnc installer

---
+++ pkg/Mint_17/tigervnc/install.sh
@@ -0,0 +1,4 @@
+cd
+wget https://bintray.com/tigervnc/stable/download_file?file_path=tigervnc-1.8.0.x86_64.tar.gz -O tigervnc-1.8.0.x86_64.tar.gz
+tar -xvzf tigervnc-1.8.0.x86_64.tar.gz
+sudo cp -r tigervnc-1.8.0.x86_64/usr/* /usr/
