44cdb4c450b99844f2575f6d9838d262d71375ce
Pablo <pablo.aledo@gmail.com>
2017-10-19 17:39:49 +0100

    sparklines

---
+++ pkg/Mint_17/sparklines/install.sh
@@ -0,0 +1,7 @@
+cd
+git clone https://github.com/deeplook/sparklines.git
+cd sparklines
+sudo pip install future
+sudo python setup.py install
+#sparklines 2 7 1 8 2 8 1 8
+#sparklines -n 2 1 2 3 4 5.0 null 3 2 1
