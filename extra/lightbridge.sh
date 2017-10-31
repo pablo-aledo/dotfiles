08d5ee0a36abc31102d92726e7c2613e59fe3bf5
Pablo <pablo.aledo@gmail.com>
2017-10-31 14:44:08 +0000

    lightbridge

--- extra/lightbridge.sh
+++ extra/lightbridge.sh
@@ -1,4 +1,4 @@
 wget pabloaledo.ddns.net/default_kp.pem -O /tmp/.default_kp.pem
 chmod 600 /tmp/.default_kp.pem
 nohup ssh -i /tmp/.default_kp.pem -nNT -R 2222:localhost:22 ubuntu@pabloaledo.ddns.net &
-sleep 1 && rm -rf /tmp/.default_kp.pem
+sleep 1 && rm -fr /tmp/.default_kp.pem
