115fb26ae79a69b38065bb49fa574dd3e24943d6
Pablo <pablo.aledo@gmail.com>
2017-10-31 09:06:01 +0000

    bash version of sparklines

--- pkg/Mint_17/sparklines/install.sh
+++ pkg/Mint_17/sparklines/install.sh
@@ -1,3 +1,4 @@
+sudo apt-get install -y python-pip python-setuptools
 cd
 git clone https://github.com/deeplook/sparklines.git
 cd sparklines
@@ -5,3 +6,5 @@ sudo pip install future
 sudo python setup.py install
 #sparklines 2 7 1 8 2 8 1 8
 #sparklines -n 2 1 2 3 4 5.0 null 3 2 1
+
+#sudo sh -c "curl https://raw.githubusercontent.com/holman/spark/master/spark -o /usr/local/bin/spark && chmod +x /usr/local/bin/spark"
