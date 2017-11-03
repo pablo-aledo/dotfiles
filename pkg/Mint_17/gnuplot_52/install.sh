a33f5b435dc7db7ba739229b857f9b21b3aa8833
Pablo <pablo.aledo@gmail.com>
2017-11-03 18:08:17 +0000

    gnuplot_52 installer

---
+++ pkg/Mint_17/gnuplot_52/install.sh
@@ -0,0 +1,6 @@
+cd
+wget https://downloads.sourceforge.net/project/gnuplot/gnuplot/5.2.0/gnuplot-5.2.0.tar.gz
+tar -xvzf gnuplot-5.2.0.tar.gz
+cd gnuplot-5.2.0
+./configure
+make && sudo make install
