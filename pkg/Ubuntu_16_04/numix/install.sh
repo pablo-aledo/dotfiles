139ccb2e46f2644e9911e6abac423117425c0b56
Pablo <pablo.aledo@gmail.com>
2017-12-03 19:44:55 +0000

    non interactive numix install

--- pkg/Ubuntu_16_04/numix/install.sh
+++ pkg/Ubuntu_16_04/numix/install.sh
@@ -1,4 +1,5 @@
-sudo add-apt-repository ppa:numix/ppa
+sudo add-apt-repository ppa:numix/ppa -y
 sudo apt-get update
-sudo apt-get install numix-gtk-theme numix-icon-theme-circle
-sudo apt-get install numix-wallpaper-*
+sudo apt-get install -y numix-gtk-theme numix-icon-theme-circle
+sudo apt-get install -y numix-wallpaper-*
+gsettings set org.gnome.desktop.interface icon-theme "Numix-Circle"
