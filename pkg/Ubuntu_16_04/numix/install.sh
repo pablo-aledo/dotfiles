a2c52860910ecc138d65d6e315d506354749ccab
Pablo <pablo.aledo@gmail.com>
2017-12-03 20:07:42 +0000

    removing installation of numix wallpapers

--- pkg/Ubuntu_16_04/numix/install.sh
+++ pkg/Ubuntu_16_04/numix/install.sh
@@ -1,5 +1,4 @@
 sudo add-apt-repository ppa:numix/ppa -y
 sudo apt-get update
 sudo apt-get install -y numix-gtk-theme numix-icon-theme-circle
-sudo apt-get install -y numix-wallpaper-*
 gsettings set org.gnome.desktop.interface icon-theme "Numix-Circle"
