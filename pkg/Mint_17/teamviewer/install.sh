8d82e168e20acb11c316b91d8d6ab33fcacef5dd
Pablo <pablo.aledo@gmail.com>
2017-12-10 16:48:30 +0000

    teamviewer using 32 bits version of libjpeg62

--- pkg/Mint_17/teamviewer/install.sh
+++ pkg/Mint_17/teamviewer/install.sh
@@ -1,4 +1,4 @@
 cd /tmp
-sudo apt-get install -y libjpeg62
+sudo apt-get install -y libjpeg62 libjpeg62:i386
 wget https://download.teamviewer.com/download/teamviewer_i386.deb
 sudo dpkg -i teamviewer_i386.deb
