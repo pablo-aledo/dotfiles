506724cd397817e7f04c26ee3837fe0f66b805cb
Pablo <pablo.aledo@gmail.com>
2017-12-11 10:03:54 +0000

    adding numix to my list of apps

---
+++ pkg/Mint_17/numix/install.sh
@@ -0,0 +1,4 @@
+sudo add-apt-repository ppa:numix/ppa -y
+sudo apt-get update
+sudo apt-get install -y numix-gtk-theme numix-icon-theme-circle
+gsettings set org.gnome.desktop.interface icon-theme "Numix-Circle"
