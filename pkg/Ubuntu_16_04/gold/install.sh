e71c60f865fc033619fceb7b5e270687758eab15
Pablo <pablo.aledo@gmail.com>
2017-12-12 18:35:04 +0000

    for gold I need sudo

--- pkg/Ubuntu_16_04/gold/install.sh
+++ pkg/Ubuntu_16_04/gold/install.sh
@@ -1,9 +1,9 @@
-update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin/ld.gold" 20
-update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin/ld.bfd" 10
+sudo update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin/ld.gold" 20
+sudo update-alternatives --install "/usr/bin/ld" "ld" "/usr/bin/ld.bfd" 10
 
-update-alternatives --config ld
+sudo update-alternatives --config ld
 
 ld --version
 
-export CPP=cpp-5 gcc-5 g++-5
-env CXXFLAGS='-march=native -flto -fuse-linker-plugin' cmake .. -DCMAKE_BUILD_TYPE=Release
+#export CPP=cpp-5 gcc-5 g++-5
+#env CXXFLAGS='-march=native -flto -fuse-linker-plugin' cmake .. -DCMAKE_BUILD_TYPE=Release
