b0e30358ca0dc5f697fc72fa477b10fabb228aff
Pablo <pablo.aledo@gmail.com>
2017-12-21 13:40:25 +0000

    gotty link

--- pkg/Mint_17/gotty/install.sh
+++ pkg/Mint_17/gotty/install.sh
@@ -1,6 +1,7 @@
 cd 
 #wget https://github.com/yudai/gotty/releases/download/pre-release/gotty_linux_amd64.tar.gz
-wget https://github.com/yudai/gotty/releases/download/pre-release/gotty_2.0.0-alpha.2_linux_amd64.tar.gz
+#wget https://github.com/yudai/gotty/releases/download/pre-release/gotty_2.0.0-alpha.2_linux_amd64.tar.gz
 #wget https://github.com/yudai/gotty/releases/download/v1.0.1/gotty_linux_amd64.tar.gz
+wget https://github.com/yudai/gotty/releases/download/v2.0.0-alpha.3/gotty_2.0.0-alpha.3_linux_amd64.tar.gz
 tar -xvzf gotty_*.tar.gz
 sudo mv ./gotty /bin
