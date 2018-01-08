82da0a86939edab99699f14a7cc1f436326ec939
Pablo <pablo.aledo@gmail.com>
2018-01-08 13:50:05 +0000

    apaxy

---
+++ pkg/Mint_17/apaxy/install.sh
@@ -0,0 +1,8 @@
+sudo cp apache2.conf /etc/apache2.conf
+cd /var/www/html/
+wget https://github.com/AdamWhitcroft/Apaxy/archive/master.zip
+unzip master.zip
+cp -r apaxy-master/apaxy/* .
+mv htaccess.txt .htaccess
+mv theme/htaccess.txt theme/.htaccess
+sudo /etc/init.d/apache2 restart
