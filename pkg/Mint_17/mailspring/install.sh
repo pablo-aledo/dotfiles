2a64d507302ac338852301b050e80cadc9f79a51
Pablo <pablo.aledo@gmail.com>
2018-02-16 14:34:01 +0000

    mailspring

---
+++ pkg/Mint_17/mailspring/install.sh
@@ -0,0 +1,4 @@
+cd /tmp/
+sudo apt-get -y install libsecret-1-dev gir1.2-gnomekeyring-1.0
+wget 'https://updates.getmailspring.com/download?platform=linuxDeb' -O mailspring.deb
+sudo dpkg -i mailspring.deb
