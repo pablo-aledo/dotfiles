ed3ff7a43db325033f403e9390c56c3e9ab14ced
Pablo <pablo.aledo@gmail.com>
2018-03-07 14:34:22 +0000

    gmvault installer

---
+++ pkg/Mint_17/gmvault/install.sh
@@ -0,0 +1,5 @@
+cd
+wget https://bitbucket.org/gaubert/gmvault-official-download/downloads/gmvault-v1.9.1-src.tar.gz
+tar -xvzf gmvault-v1.9.1-src.tar.gz
+cd gmvault-1.9.1
+sudo python ./setup.py install
