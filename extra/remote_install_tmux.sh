c0fbd989b6fd45b1a29f4ea41f2563edba4b97bb
Pablo <pablo.aledo@gmail.com>
2018-03-21 14:38:00 +0000

    remote_install_tmux for installing in foreign computers

--- extra/remote_install_tmux.sh
+++ extra/remote_install_tmux.sh
@@ -6,14 +6,12 @@ cd    .dotfiles_pga
 
 wget https://raw.githubusercontent.com/pablo-aledo/dotfiles/master/source/.shell/pkg -O .pkg
 
-echo 'pkg update'        >> .pkg # requires superuser
-echo 'pkg install zsh'   >> .pkg # requires superuser
-echo 'pkg install git'   >> .pkg # requires superuser
-echo 'pkg install unzip' >> .pkg # requires superuser
 echo 'git clone https://github.com/pablo-aledo/dotfiles.git .dotfiles' >> .pkg
 echo 'cd .dotfiles'      >> .pkg
 echo 'bash ./install.sh' >> .pkg
 echo 'touch ~/.paths'      >> .pkg
+
+sed -i s/^HOME=/HOME=$PWD/g .dotfiles/install.sh
 
 bash .pkg
 
