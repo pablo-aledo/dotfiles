2a7f7fa3aa2d539caadd6b889dae4305a9d65a80
Pablo <pablo.aledo@gmail.com>
2018-03-26 06:32:19 +0000

    novnc3 server

---
+++ pkg/Mint_17/novnc3/install.sh
@@ -0,0 +1,13 @@
+mkdir ~/.vnc 
+cp passwd ~/.vnc/
+cp xstartup ~/.vnc/
+chmod +x ~/.vnc/xstartup
+
+cd
+wget https://bintray.com/tigervnc/stable/download_file?file_path=tigervnc-1.8.0.x86_64.tar.gz -O tigervnc-1.8.0.x86_64.tar.gz
+tar -xvzf tigervnc-1.8.0.x86_64.tar.gz
+sudo cp -r tigervnc-1.8.0.x86_64/usr/* /usr/
+
+cd
+git clone https://github.com/novnc/noVNC.git
+
