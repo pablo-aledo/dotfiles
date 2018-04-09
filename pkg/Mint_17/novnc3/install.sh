e965b94837382940af019263772ccb47f2366cf3
Pablo <pablo.aledo@gmail.com>
2018-04-09 07:30:44 +0000

    add x11-xkb-utils to novnc3

--- pkg/Mint_17/novnc3/install.sh
+++ pkg/Mint_17/novnc3/install.sh
@@ -7,6 +7,7 @@ cd
 wget https://bintray.com/tigervnc/stable/download_file?file_path=tigervnc-1.8.0.x86_64.tar.gz -O tigervnc-1.8.0.x86_64.tar.gz
 tar -xzf tigervnc-1.8.0.x86_64.tar.gz
 sudo cp -r tigervnc-1.8.0.x86_64/usr/* /usr/
+sudo apt-get install x11-xkb-utils
 
 cd
 git clone https://github.com/novnc/noVNC.git
