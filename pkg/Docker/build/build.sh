b009bc8bc3bfa3b97f7101a89b9cbac94cfc6862
Pablo <pablo.aledo@gmail.com>
2018-04-19 11:05:12 +0000

    running script for build

---
+++ pkg/Docker/build/build.sh
@@ -0,0 +1,31 @@
+if [ -e runme.sh ]
+then
+    chmod +x runme.sh
+    ./runme.sh
+elif [ -e CMakeLists.txt ]
+then
+    mkdir build
+    cd build
+    cmake ..
+    make
+    make install
+elif [ -e configure ]
+then
+    ./configure
+    make
+    make install
+elif [ -e Makefile ]
+then
+    make
+elif [ -e Makefile.am ]
+then
+    autoreconf -i
+    automake
+    autoconf
+    ./configure
+    make
+    make install
+else
+     echo "Unknown structure"
+fi
+
