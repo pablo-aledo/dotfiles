94c8ccffc7bfb16bac1d3b48e883f431d20e5cf5
Pablo <pablo.aledo@gmail.com>
2018-04-25 10:53:36 +0000

    build with a parameter executes that

--- pkg/Docker/build/build.sh
+++ pkg/Docker/build/build.sh
@@ -1,31 +1,35 @@
-if [ -e runme.sh ]
+if [ $# -eq 0 ]
 then
-    chmod +x runme.sh
-    ./runme.sh
-elif [ -e CMakeLists.txt ]
-then
-    mkdir build
-    cd build
-    cmake ..
-    make
-    #make install
-elif [ -e configure ]
-then
-    ./configure
-    make
-    #make install
-elif [ -e Makefile ]
-then
-    make
-elif [ -e Makefile.am ]
-then
-    autoreconf -i
-    automake
-    autoconf
-    ./configure
-    make
-    #make install
+	if [ -e runme.sh ]
+	then
+	    chmod +x runme.sh
+	    ./runme.sh
+	elif [ -e CMakeLists.txt ]
+	then
+	    mkdir build
+	    cd build
+	    cmake ..
+	    make
+	    #make install
+	elif [ -e configure ]
+	then
+	    ./configure
+	    make
+	    #make install
+	elif [ -e Makefile ]
+	then
+	    make
+	elif [ -e Makefile.am ]
+	then
+	    autoreconf -i
+	    automake
+	    autoconf
+	    ./configure
+	    make
+	    #make install
+	else
+	     echo "Unknown structure"
+	fi
 else
-     echo "Unknown structure"
+	sh -c "$*"
 fi
-
