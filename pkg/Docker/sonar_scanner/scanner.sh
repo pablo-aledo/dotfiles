5c3e6584ea7af480523b6261021ae0ce1e323e77
Pablo <pablo.aledo@gmail.com>
2018-05-14 16:15:21 +0000

    sonar_server and sonar_scanner

---
+++ pkg/Docker/sonar_scanner/scanner.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+until curl -s -X GET http://${SONAR_IP}:9000/api/system/status | grep UP;
+  do echo "Waiting SonarQube server..."
+  sleep 5
+done
+
+find . -name '*.cpp' -o -name '*.hpp' -o -name '*.cc' -o -name '*.hh' -o -name '*.c' -o -name '*.h' | while read file
+do
+  if echo $file | egrep "test" > /dev/null; then
+    tests="$tests,$file"
+  else
+    sources="$sources,$file"
+  fi
+done
+
+/usr/share/sonar-scanner-3.0.3.778-linux/bin/sonar-scanner \
+  -D sonar.host.url=http://${SONAR_IP}:9000 \
+  -D sonar.projectKey=${PROJECT_NAME} \
+  -D sonar.projectName=${PROJECT_NAME} \
+  -D sonar.projectVersion=1.0 \
+  -D sonar.sources=$sources \
+  -D sonar.tests=$tests \
+  -D sonar.exclusions=**/*.pb.h,**/*.pb.cc,**/*.capnp.c++,**/*.capnp.h,**/.release/**.*,**/.debug/**.*,CMakeLists.txt,**/vendor/** \
+  -D sonar.cxx.cppcheck.reportPath=./.cppcheck.xml \
+  -D sonar.cxx.valgrind.reportPath=./.valgrind.*.xml \
+  -D sonar.cxx.xunit.reportPath=./.gtest.*.xml \
+  -D sonar.cxx.coverage.reportPath=./.gcov.xml \
+  -D sonar.cxx.jsonCompilationDatabase=/workdir/compile_commands.json \
+  -D sonar.golint.reportPath=./.golint.xml \
+  -D sonar.coverage.reportPath=./.gocov.xml \
+  -D sonar.test.reportPath=./.gotest.xml \
+  -X
+
