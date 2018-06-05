992ac2afeeafa66b87c76cd7ebe923ed4805cc18
Pablo <pablo.aledo@gmail.com>
2018-06-05 08:47:58 +0000

    codecheck script for current folder

--- pkg/Mint_17/codechecker/codecheck.sh
+++ pkg/Mint_17/codechecker/codecheck.sh
@@ -15,8 +15,8 @@ export PATH=/usr/share/clang/scan-build-py-3.9/bin:$PATH
 # NOTE: SKIP this line if clang is available in your PATH as an installed Linux package.
 #export PATH=~/<user path>/build/bin:$PATH
 
-CodeChecker check -b "cd ~/codechecker/tests/projects/single_bug/ && make clean && make" -o ~/results
+CodeChecker check -b "cd $PWD && make clean && make" -o ~/results
 CodeChecker server &
 CodeChecker store ~/results -n my-project
-google-chrome localhost:8001
+google-chrome 'http://localhost:8001'
 
