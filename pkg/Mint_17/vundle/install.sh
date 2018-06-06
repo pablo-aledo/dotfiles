9865cb96fea446d69d5dccce57253893cc5cc147
Pablo <pablo.aledo@gmail.com>
2018-06-06 17:31:16 +0000

    adding vundle plugins

--- pkg/Mint_17/vundle/install.sh
+++ pkg/Mint_17/vundle/install.sh
@@ -19,16 +19,55 @@ Plugin 'VundleVim/Vundle.vim'
 " The following are examples of different formats supported.
 " Keep Plugin commands between vundle#begin/end.
 " plugin on GitHub repo
+
+Plugin 'vim-airline/vim-airline'
+Plugin 'vim-scripts/Align'
+Plugin 'vim-scripts/AnsiEsc'
+Plugin 'Town/vim-autoclose'
+Plugin 'vim-scripts/a'
+Plugin 'Rip-Rip/clang_complete'
+Plugin 'vim-scripts/Conque-Shell'
+Plugin 'kien/ctrlp'
+Plugin 'Twinside/vim-cuteErrorMarker'
+Plugin 'c.vim'
+Plugin 'fadein/vim-FIGlet'
 Plugin 'tpope/vim-fugitive'
+Plugin 'sjl/gundo.vim'
+Plugin 'tmhedberg/matchit'
+Plugin 'severin-lemaignan/vim-minimap'
+Plugin 'scrooloose/nerdcommenter'
+Plugin 'scrooloose/nerdtree'
+Plugin 'vim-scripts/OmniCppComplete'
+Plugin 'kien/rainbow_parentheses.vim'
+Plugin 'LucHermitte/vim-refactor'
+Plugin 'derekwyatt/vim-scala'
+Plugin 'vim-scripts/SearchComplete'
+Plugin 'vim-scripts/self.vim'
+Plugin 'garbas/vim-snipmate'
+Plugin 'tpope/vim-surround'
+Plugin 'vim-syntastic/syntastic'
+Plugin 'majutsushi/tagbar'
+Plugin 'taglist.vim'
+Plugin 'edkolev/tmuxline.vim'
+Plugin 'jgdavey/tslime.vim'
+Plugin 'vim-scripts/AutoComplPop'
+Plugin 'gyim/vim-boxdraw'
+Plugin 'ryanoasis/vim-devicons'
+Plugin 'tpope/vim-dispatch'
+Plugin 'airblade/vim-gitgutter'
+Plugin 'vim-latex/vim-latex'
+Plugin 'triglav/vim-visual-increment'
+
+" Plugin 'tpope/vim-fugitive'
 " plugin from http://vim-scripts.org/vim/scripts.html
 " Plugin 'L9'
 " Git plugin not hosted on GitHub
-Plugin 'git://git.wincent.com/command-t.git'
+" Plugin 'git://git.wincent.com/command-t.git'
 " git repos on your local machine (i.e. when working on your own plugin)
-Plugin 'file:///home/gmarik/path/to/plugin'
+" Plugin 'file:///home/gmarik/path/to/plugin'
 " The sparkup vim script is in a subdirectory of this repo called vim.
 " Pass the path to set the runtimepath properly.
-Plugin 'rstacruz/sparkup', {'rtp': 'vim/'}
+" Plugin 'rstacruz/sparkup', {'rtp': 'vim/'}
 " Install L9 and avoid a Naming conflict if you've already installed a
 " different version somewhere else.
 " Plugin 'ascenator/L9', {'name': 'newL9'}
