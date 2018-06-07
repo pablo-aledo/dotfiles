9e390e238f4926f831e1272a6e4ef509073ab92a
Pablo <pablo.aledo@gmail.com>
2018-06-07 11:01:25 +0200

    getting vundle closer to ycm

--- pkg/Mint_17/vundle/install.sh
+++ pkg/Mint_17/vundle/install.sh
@@ -1,4 +1,5 @@
-(cd /tmp && bak ~/.vimrc ~/.vim && rm -rf ~/.vimrc ~/.vim )
+#(cd /tmp && bak ~/.vimrc ~/.vim && rm -rf ~/.vimrc ~/.vim )
+rm -rf ~/.vimrc ~/.vim
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
@@ -9,7 +10,8 @@ do
 	echo source $a >> ~/.vimrc
 done
 
-vim +PluginInstall +qall
+vim +PluginInstall
+#vim +PluginInstall +qall
 
 mkdir -p ~/.vim
 cd ~/.vim
@@ -29,3 +31,15 @@ do
 	mkdir -p $(dirname $dst)
 	ln -sf $src $dst
 done
+
+if [ -e ~/.vim/bundle/YouCompleteMe ]
+then
+	sudo apt-get install build-essential cmake
+	sudo apt-get install python-dev python3-dev
+
+	cd ~/.vim/bundle/YouCompleteMe
+	git submodule update --init --recursive
+	./install.py --clang-completer
+	cd -
+fi
+
