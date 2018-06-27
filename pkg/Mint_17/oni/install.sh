a33f311d3d2aa60607f51b823f82c3cb7473c356
Pablo <pablo.aledo@gmail.com>
2018-06-27 09:34:58 +0000

    improving oni installer

--- pkg/Mint_17/oni/install.sh
+++ pkg/Mint_17/oni/install.sh
@@ -1,8 +1,10 @@
+sudo apt-get install software-properties-common
+
 sudo apt-add-repository ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim
 
 cd /tmp
-sudo apt-get install libappindicator1
+sudo apt-get install libappindicator1 gconf2 gconf-service libnotify4 libxtst6 libnss3 libxss1
 wget https://github.com/onivim/oni/releases/download/v0.3.6/Oni-0.3.6-amd64-linux.deb
 sudo dpkg -i Oni-0.3.6-amd64-linux.deb
