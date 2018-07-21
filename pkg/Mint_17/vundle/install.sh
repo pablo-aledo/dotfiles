96bad341c0c0504242bb57d0793d3bb95f56ae95
Pablo <pablo.aledo@gmail.com>
2018-07-21 07:58:53 +0200

    cheat vim plugin

--- pkg/Mint_17/vundle/install.sh
+++ pkg/Mint_17/vundle/install.sh
@@ -1,6 +1,5 @@
 #(cd /tmp && bak ~/.vimrc ~/.vim && rm -rf ~/.vimrc ~/.vim )
 rm -rf ~/.vimrc ~/.vim
-
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 echo source $PWD/vundle > ~/.vimrc
