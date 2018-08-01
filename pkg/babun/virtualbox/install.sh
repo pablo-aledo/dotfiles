47052e72ed9c54bb648e6a2a90de5a0857b5cd06
Pablo <pablo.aledo@gmail.com>
2018-08-01 10:22:34 +0000

    a few installers for windows

--- pkg/babun/virtualbox/install.sh
+++ pkg/babun/virtualbox/install.sh
@@ -1,4 +1,4 @@
 cd /tmp
 wget https://download.virtualbox.org/virtualbox/5.2.16/VirtualBox-5.2.16-123759-Win.exe
 chmod +x VirtualBox-5.2.16-123759-Win.exe
-open VirtualBox-5.2.16-123759-Win.exe
+cygstart VirtualBox-5.2.16-123759-Win.exe
