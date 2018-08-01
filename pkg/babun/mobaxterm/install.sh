c1b7832ec8022661cec197b509673646f96193a1
Pablo <pablo.aledo@gmail.com>
2018-08-01 09:37:34 +0000

    a few installers for windows

---
+++ pkg/babun/mobaxterm/install.sh
@@ -0,0 +1,4 @@
+cd /tmp
+wget https://download.mobatek.net/1082018070240950/MobaXterm_Installer_v10.8.zip
+unzip MobaXterm_Installer_v10.8.zip
+open MobaXterm_installer_10.8.msi
---
+++ pkg/babun/virtualbox/install.sh
@@ -0,0 +1,3 @@
+cd /tmp
+wget https://download.virtualbox.org/virtualbox/5.2.16/VirtualBox-5.2.16-123759-Win.exe
+open VirtualBox-5.2.16-123759-Win.exe
