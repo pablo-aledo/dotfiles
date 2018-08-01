805622f34bbacdfe37f94eaf89ff030194770826
U-INDRA\pgonzalez <pgonzalez@PGONZALEZ10.indra.es>
2018-08-01 14:34:35 +0200

    Some windows installers

---
+++ pkg/babun/caffeine/install.sh
@@ -0,0 +1,6 @@
+cd /tmp
+wget http://www.zhornsoftware.co.uk/caffeine/caffeine.zip
+unzip caffeine.zip
+mkdir -p /c/Users/$USER/Downloads/Programas/caffeine.exe
+cp caffeine.exe /c/Users/$USER/Downloads/Programas/caffeine.exe
+
---
+++ pkg/babun/meld/install.sh
@@ -0,0 +1,3 @@
+cd /tmp
+wget https://download.gnome.org/binaries/win32/meld/3.18/Meld-3.18.2-win32.msi
+cygstart Meld-3.18-win32.msi
