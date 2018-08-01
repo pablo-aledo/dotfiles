0d626c39c86511803270a951f4c1c926916725e6
Pablo <pablo.aledo@gmail.com>
2018-08-01 10:18:18 +0000

    a few installers for windows

--- pkg/babun/npp/install.sh
+++ pkg/babun/npp/install.sh
@@ -1,3 +1,4 @@
 cd /tmp
 wget https://notepad-plus-plus.org/repository/7.x/7.5.8/npp.7.5.8.Installer.exe
+chmod +x npp.7.5.8.Installer.exe
 open npp.7.5.8.Installer.exe
--- pkg/babun/virtualbox/install.sh
+++ pkg/babun/virtualbox/install.sh
@@ -1,3 +1,4 @@
 cd /tmp
 wget https://download.virtualbox.org/virtualbox/5.2.16/VirtualBox-5.2.16-123759-Win.exe
+chmod +x VirtualBox-5.2.16-123759-Win.exe
 open VirtualBox-5.2.16-123759-Win.exe
