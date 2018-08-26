07ad88637f505725671fcfde135b3cc00d9a42b1
Pablo <pablo.aledo@gmail.com>
2018-08-26 15:06:18 +0000

    tools to parse html and xml

--- pkg/Mint_17/xpup/install.sh
+++ pkg/Mint_17/xpup/install.sh
@@ -2,5 +2,6 @@ sudo apt-get install golang
 export GOPATH=$HOME/gopath
 mkdir $GOPATH
 go get github.com/ericchiang/xpup
-echo path $GOPATH/bin >> ~/.paths
+echo 'export GOPATH=$HOME/gopath'    >> ~/.paths
+echo 'export PATH=$GOPATH/bin:$PATH' >> ~/.paths
 source ~/.paths
