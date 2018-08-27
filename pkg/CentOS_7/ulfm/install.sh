9fc87e597785e5663975f0e7aeae53f3b83e0bec
Pablo <pablo.aledo@gmail.com>
2018-08-27 11:38:59 +0000

    fixing install of ulfm

--- pkg/CentOS_7/ulfm/install.sh
+++ pkg/CentOS_7/ulfm/install.sh
@@ -1,3 +1,4 @@
+sudo yum install -y perl-Data-Dumper.x86_64 autoconf libtool gcc-c++ flex bison
 cd
 git clone https://bitbucket.org/icldistcomp/ulfm2.git
 cd ulfm2
@@ -5,4 +6,5 @@ cd ulfm2
 ./configure --with-ft
 #    use --with-ft (default) to enable ULFM,
 #        --without-ft to disable it
-make all install
+make all
+sudo make install
