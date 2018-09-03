c882309c310ab081cb055f55bf6ed5f168f105cd
Pablo <pablo.aledo@gmail.com>
2018-09-03 09:22:53 +0000

    noti

--- pkg/Mint_17/noti/install.sh
+++ pkg/Mint_17/noti/install.sh
@@ -5,4 +5,5 @@
 # echo 'export GOPATH=$HOME/gopath'    >> ~/.paths
 # echo 'export PATH=$GOPATH/bin:$PATH' >> ~/.paths
 # source ~/.paths
-curl -L $(curl -s https://api.github.com/repos/variadico/noti/releases/latest | awk '/browser_download_url/ { print $2 }' | grep 'linux-amd64' | sed 's/"//g') | tar -xz
+cd /bin
+curl -L $(curl -s https://api.github.com/repos/variadico/noti/releases/latest | awk '/browser_download_url/ { print $2 }' | grep 'linux-amd64' | sed 's/"//g') | sudo tar -xz
