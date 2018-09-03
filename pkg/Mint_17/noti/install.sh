5db4dba422abad30057ab3ab21dcb6370e5e2867
Pablo <pablo.aledo@gmail.com>
2018-09-03 09:20:15 +0000

    noti

--- pkg/Mint_17/noti/install.sh
+++ pkg/Mint_17/noti/install.sh
@@ -1,7 +1,8 @@
-sudo apt-get install golang
-export GOPATH=$HOME/gopath
-mkdir $GOPATH
-go get -u github.com/variadico/noti/cmd/noti
-echo 'export GOPATH=$HOME/gopath'    >> ~/.paths
-echo 'export PATH=$GOPATH/bin:$PATH' >> ~/.paths
-source ~/.paths
+# sudo apt-get install golang
+# export GOPATH=$HOME/gopath
+# mkdir $GOPATH
+# go get -u github.com/variadico/noti/cmd/noti
+# echo 'export GOPATH=$HOME/gopath'    >> ~/.paths
+# echo 'export PATH=$GOPATH/bin:$PATH' >> ~/.paths
+# source ~/.paths
+curl -L $(curl -s https://api.github.com/repos/variadico/noti/releases/latest | awk '/browser_download_url/ { print $2 }' | grep 'linux-amd64' | sed 's/"//g') | tar -xz
