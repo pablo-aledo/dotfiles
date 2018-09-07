59dc6cbf21a46d59fa19bf5511cb4f812982926e
Pablo <pablo.aledo@gmail.com>
2018-09-07 08:02:30 +0000

    start docker after install in centos

--- pkg/CentOS_7/docker_ce/install.sh
+++ pkg/CentOS_7/docker_ce/install.sh
@@ -5,3 +5,5 @@ sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
 sudo yum install docker-ce
+
+sudo systemctl start docker
