bf40b49caefba1db5f52ff5171b17dd76eba026d
Pablo <pablo.aledo@gmail.com>
2018-10-09 15:40:27 +0000

    fix bug in nfsserver

--- pkg/Ubuntu_16_04/nfs/install.sh
+++ pkg/Ubuntu_16_04/nfs/install.sh
@@ -1,4 +1,4 @@
 sudo apt-get install -y nfs-kernel-server nfs-common portmap
 sudo mkdir /mnt/nfs
-echo '192.168.1.0/24(ro,no_root_squash,insecure)' |  sudo tee -a /etc/exports
+echo '/mnt/nfs 192.168.1.0/24(ro,no_root_squash,insecure)' |  sudo tee -a /etc/exports
 sudo service nfs-server restart
