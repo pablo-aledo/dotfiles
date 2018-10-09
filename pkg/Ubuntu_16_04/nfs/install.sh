443092dbf630e6b9d87f7442300506f4d0ef0cd9
Pablo <pablo.aledo@gmail.com>
2018-10-09 15:28:13 +0000

    nfs server

---
+++ pkg/Ubuntu_16_04/nfs/install.sh
@@ -0,0 +1,4 @@
+sudo apt-get install -y nfs-kernel-server nfs-common portmap
+sudo mkdir /mnt/nfs
+echo '192.168.1.0/24(ro,no_root_squash,insecure)' |  sudo tee -a /etc/exports
+sudo service nfs-server restart
