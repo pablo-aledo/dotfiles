75f4034df4410aefcfdcd293697ce031f00c2c31
Pablo <pablo.aledo@gmail.com>
2018-10-17 10:06:33 +0000

    install docker_ce without confirmation

--- pkg/Ubuntu_16_04/docker_ce/install.sh
+++ pkg/Ubuntu_16_04/docker_ce/install.sh
@@ -2,7 +2,7 @@ sudo apt-get remove docker docker-engine docker.io
 
 sudo apt-get update
 
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -10,11 +10,11 @@ sudo apt-get install \
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
-sudo add-apt-repository \
+sudo add-apt-repository -y \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
 sudo apt-get update
 
-sudo apt-get install docker-ce
+sudo apt-get install -y docker-ce
