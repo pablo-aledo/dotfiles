3860664fbad02bf470e2b06ce5f290d00e76846c
Pablo <pablo.aledo@gmail.com>
2018-10-18 07:16:58 +0000

    add kubespy

---
+++ pkg/Mint_17/kubespy/install.sh
@@ -0,0 +1,5 @@
+cd /tmp
+wget https://github.com/pulumi/kubespy/releases/download/v0.4.0/kubespy-linux-amd64.tar.gz
+tar -xvzf kubespy-linux-amd64.tar.gz
+sudo mv releases/kubespy-linux-amd64/kubespy /bin
+sudo chmod +x /bin/kubespy
