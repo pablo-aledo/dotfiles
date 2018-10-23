d7c0e614f9ae450ba7890005af1ffdf87bbfa75f
Pablo <pablo.aledo@gmail.com>
2018-10-23 15:50:00 +0200

    oc installer

---
+++ pkg/CentOS_7/oc/install.sh
@@ -0,0 +1,5 @@
+cd /tmp/
+wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+tar -xvzf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+cd openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit
+sudo cp oc /usr/local/bin
