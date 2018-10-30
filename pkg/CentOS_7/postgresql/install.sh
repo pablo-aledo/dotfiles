044bbe688656f87c8ae01172dd11d057a442ccb1
Pablo <pablo.aledo@gmail.com>
2018-10-30 15:38:11 +0000

    fix postgresql installer

--- pkg/CentOS_7/postgresql/install.sh
+++ pkg/CentOS_7/postgresql/install.sh
@@ -3,5 +3,5 @@ sudo yum-config-manager --enable rhel-server-rhscl-7-rpms
 sudo yum install rh-postgresql10
 
 scl enable rh-postgresql10 bash
-postgresql-setup --initdb
+sudo /opt/rh/rh-postgresql10/root/usr/bin/postgresql-setup --initdb
 service rh-postgresql10-postgresql start
