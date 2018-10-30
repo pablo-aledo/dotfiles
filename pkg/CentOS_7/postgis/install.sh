b5777d6ed158bf9a9350a8cc60ea8adeb1ed14ee
Pablo <pablo.aledo@gmail.com>
2018-10-30 09:02:36 +0000

    finish postgis installation

--- pkg/CentOS_7/postgis/install.sh
+++ pkg/CentOS_7/postgis/install.sh
@@ -1,10 +1,9 @@
-#https://tecadmin.net/install-postgresql-server-centos/
-
-#sudo yum install -y "https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-centos10-10-2.noarch.rpm"
+sudo yum install -y "https://download.postgresql.org/pub/repos/yum/10/redhat/rhel-7-x86_64/pgdg-centos10-10-2.noarch.rpm"
+sudo yum install -y "https://timescalereleases.blob.core.windows.net/rpm/timescaledb-0.12.1-postgresql-10-0.x86_64.rpm"
 sudo yum install -y postgis25_10
 
 sudo /usr/pgsql-10/bin/postgresql-10-setup initdb
 
-echo "shared_preload_libraries = 'timescaledb,postgis'" | sudo tee -a /var/lib/pgsql/10/data/postgresql.conf
+echo "shared_preload_libraries = 'timescaledb'" | sudo tee -a /var/lib/pgsql/10/data/postgresql.conf
 
 sudo systemctl start postgresql-10.service
