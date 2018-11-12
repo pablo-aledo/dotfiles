fe392150bc7cdc01f32b2bf64fd5a6f83be30744
Pablo <pablo.aledo@gmail.com>
2018-11-12 09:28:26 +0100

    go added to path

--- pkg/Ubuntu_16_04/golang19/install.sh
+++ pkg/Ubuntu_16_04/golang19/install.sh
@@ -1,3 +1,5 @@
 wget https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz -O - | sudo tar -zxv -C /usr/local/
 mkdir ~/go
 echo 'export GOPATH=~/go' >> ~/.paths
+echo 'export PATH=/usr/local/go/bin:$PATH' >> ~/.paths
+source ~/.paths
