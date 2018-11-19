6b97916bf9eba04057c83f55bee22223e73dcf58
Pablo <pablo.aledo@gmail.com>
2018-11-19 11:55:03 +0100

    add npm install from webpage

---
+++ pkg/CentOS_7/npm/install.sh
@@ -0,0 +1,5 @@
+sudo mkdir -p /usr/src/node; sudo chmod 777 /usr/src/node; cd /usr/src/node
+wget https://nodejs.org/dist/v10.13.0/node-v10.13.0-linux-x64.tar.xz
+tar -xvJf node-v10.13.0-linux-x64.tar.xz
+cd node-v10.13.0-linux-x64
+sudo ln -s bin/* /usr/local/bin
