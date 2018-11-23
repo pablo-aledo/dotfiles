f9bf9d9b9e1f9d0f157ab71fb272778d1a60bdbc
Pablo <pablo.aledo@gmail.com>
2018-11-23 13:02:22 +0000

    neovim

---
+++ pkg/Ubuntu_16_04/neovim/install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install software-properties-common
+sudo apt-get install python-software-properties
+
+sudo apt-add-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
