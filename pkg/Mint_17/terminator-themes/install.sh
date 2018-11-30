c7a12a7a70f58c731b638cf42d1fbf18dc287820
Pablo <pablo.aledo@gmail.com>
2018-11-30 11:44:28 +0000

    terminator-themes

---
+++ pkg/Mint_17/terminator-themes/install.sh
@@ -0,0 +1,4 @@
+sudo apt-get install -y python-pip
+pip install requests
+mkdir -p $HOME/.config/terminator/plugins
+wget https://git.io/v5Zwz -O $HOME/.config/terminator/plugins/terminator-themes.py
