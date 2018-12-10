328fb4b0ace27eec51952ed1c9b593143701cda6
Pablo <pablo.aledo@gmail.com>
2018-12-10 08:36:05 +0000

    use source instead of bash to keep env variables

--- remote_install.sh
+++ remote_install.sh
@@ -12,7 +12,7 @@ echo 'pkg install sudo'  >> .pkg # requires superuser
 echo 'pkg install grep'  >> .pkg # requires superuser
 echo 'git clone https://github.com/pablo-aledo/dotfiles.git .dotfiles' >> .pkg
 echo 'cd .dotfiles'      >> .pkg
-echo 'bash ./install.sh' >> .pkg
+echo 'source ./install.sh' >> .pkg
 echo 'touch ~/.paths'      >> .pkg
 
-bash .pkg
+source .pkg
