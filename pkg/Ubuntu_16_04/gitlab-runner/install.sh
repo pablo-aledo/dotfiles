7f13384d84cc82ba1f3d402ae6683999158066a6
Pablo <pablo.aledo@gmail.com>
2018-12-17 15:42:00 +0000

    add gitlab-runner alias

--- pkg/Ubuntu_16_04/gitlab-runner/install.sh
+++ pkg/Ubuntu_16_04/gitlab-runner/install.sh
@@ -1,2 +1,5 @@
 sudo wget -O  /usr/local/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
 sudo chmod +x /usr/local/bin/gitlab-runner
+sudo useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
+sudo gitlab-runner install --user gitlab-runner --working-directory=/home/gitlab-runner
+sudo gitlab-runner start
