f49a1acb39a5ae29a891178bf36bf109cb2afc65
Pablo <pablo.aledo@gmail.com>
2019-01-17 09:06:26 +0000

    openshift installer

---
+++ pkg/Ubuntu_16_04/oc/install.sh
@@ -0,0 +1,3 @@
+cd
+wget https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-server-v3.11.0-0cbc58b-linux-64bit.tar.gz
+tar -xvzf openshift-origin-server-v3.11.0-0cbc58b-linux-64bit.tar.gz
