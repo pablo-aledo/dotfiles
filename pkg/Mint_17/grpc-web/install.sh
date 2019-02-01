8932bc81421a4df18ee775f7861980499a71254b
Pablo <pablo.aledo@gmail.com>
2019-02-01 10:21:07 +0000

    improve grpc-web installer

--- pkg/Mint_17/grpc-web/install.sh
+++ pkg/Mint_17/grpc-web/install.sh
@@ -1,5 +1,7 @@
+#pkg install protobuf3
+sudo apt-get install -y make g++
+
 sudo mkdir -p /usr/src/grpc-web && sudo chmod 777 /usr/src/grpc-web
 cd /usr/src/grpc-web
-git clone https://github.com/grpc/grpc-web
-cd grpc-web
+git clone https://github.com/grpc/grpc-web .
 sudo make install-plugin
