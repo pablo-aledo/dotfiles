884e9dc4c64ff8db3508ab40d84c8da15e2aa885
Pablo <pablo.aledo@gmail.com>
2019-04-10 10:02:25 +0000

    fix bug in cmake installer

--- pkg/Ubuntu_16_04/cmake/install.sh
+++ pkg/Ubuntu_16_04/cmake/install.sh
@@ -1,5 +1,5 @@
 cd
 wget https://cmake.org/files/v3.4/cmake-3.4.1-Linux-x86_64.tar.gz
 tar xf cmake-3.4.1-Linux-x86_64.tar.gz
-export PATH="`pwd`/cmake-3.4.1-Linux-x86_64/bin:$PATH" # save it in .bashrc if needed
-echo 'export PATH="`pwd`/cmake-3.4.1-Linux-x86_64/bin:$PATH"' >> ~/.paths
+export PATH="$HOME/cmake-3.4.1-Linux-x86_64/bin:$PATH" # save it in .bashrc if needed
+echo 'export PATH="~/cmake-3.4.1-Linux-x86_64/bin:$PATH"' | sed "s|~|$HOME|g" >> ~/.paths
