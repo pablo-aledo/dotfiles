084b1b77b42e0dc2859d808e400ca85ab5fa733e
Pablo <pablo.aledo@gmail.com>
2019-04-10 10:51:10 +0200

    option to skip in s2link

--- shortcuts/s2link/init.sh
+++ shortcuts/s2link/init.sh
@@ -4,5 +4,9 @@
 [ $# -ge 2 ] && query=$2
 [ $# -ge 2 ] || query="is: starred avxhm.se"
 
+[ $# -ge 3 ] && skip=$3
+[ $# -ge 3 ] || skip=0
+
 echo $query > /tmp/query
 echo $count > /tmp/count
+echo $skip > /tmp/skip
