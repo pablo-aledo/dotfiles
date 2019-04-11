31b489fce80a2be196d3632dc97863e9c83f01ea
Pablo <pablo.aledo@gmail.com>
2019-04-11 08:05:30 +0000

    change version of boost

--- pkg/CentOS_7/boost-devel/install.sh
+++ pkg/CentOS_7/boost-devel/install.sh
@@ -1,7 +1,7 @@
 cd /tmp
-wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz -O boost.tar.gz
+wget https://dl.bintray.com/boostorg/release/1.69.0/source/boost_1_69_0.tar.gz -O boost.tar.gz
 tar -zxf boost.tar.gz
-cd boost_1_65_1
+cd boost_1_69_0
 ./bootstrap.sh
 sudo ./b2 install
 
