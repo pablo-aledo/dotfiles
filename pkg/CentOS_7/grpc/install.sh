91577805c2d9909eae553bae4af8a7737df547dc
Pablo <pablo.aledo@gmail.com>
2019-04-11 08:46:53 +0000

    Fix installation errors in grpc. Generate cmakefiles

--- pkg/CentOS_7/grpc/install.sh
+++ pkg/CentOS_7/grpc/install.sh
@@ -1,11 +1,17 @@
-sudo apt-get install build-essential autoconf libtool
-
 sudo mkdir -p /usr/src/grpc && sudo chmod 777 /usr/src/grpc
 cd /usr/src/grpc
 git clone https://github.com/grpc/grpc.git .
+git checkout 7e6b5db90de63c820e80440502aa1427bcd72ed9
 git submodule update --init
 
 mkd build
+cmake3 ..
+
+sed -i 's/gRPC_ZLIB_PROVIDER:STRING=.*/gRPC_ZLIB_PROVIDER:STRING=package/g' CMakeCache.txt
+sed -i 's/gRPC_CARES_PROVIDER:STRING=.*/gRPC_CARES_PROVIDER:STRING=package/g' CMakeCache.txt
+sed -i 's/gRPC_SSL_PROVIDER:STRING=.*/gRPC_SSL_PROVIDER:STRING=package/g' CMakeCache.txt
+sed -i 's/gRPC_PROTOBUF_PROVIDER:STRING=.*/gRPC_PROTOBUF_PROVIDER:STRING=package/g' CMakeCache.txt
+
 cmake3 ..
 make
 sudo make install
