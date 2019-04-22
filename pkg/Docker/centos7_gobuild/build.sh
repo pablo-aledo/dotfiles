74208cae70bf5956328dce818fe71f5d5b8f639f
Pablo <pablo.aledo@gmail.com>
2019-04-22 08:20:52 +0000

    Fix build in centos7_gobuild

--- pkg/Docker/centos7_gobuild/build.sh
+++ pkg/Docker/centos7_gobuild/build.sh
@@ -2,50 +2,50 @@
 
 if [ $# -eq 0 ]
 then
-	[ -e .git ] && git submodule update --init --recursive
+    [ -e .git ] && git submodule update --init --recursive
 
-	if [ -e runme.sh ]
-	then
-	    chmod +x runme.sh
-	    ./runme.sh
-    elif [ "$(find -iname compile_project.sh 2>/dev/null)" ]
+    if [ -e runme.sh ]
+    then
+        chmod +x runme.sh
+        ./runme.sh
+    elif [ "$(find -iname compile_project.sh 2>/dev/null)" != "" ]
     then
         (source "$(find -iname compile_project.sh)")
         (source "$(find -iname test_project.sh)")
-	elif [ -e CMakeLists.txt ]
-	then
-	    mkdir build
-	    cd build
+    elif [ -e CMakeLists.txt ]
+    then
+        mkdir build
+        cd build
         export CMAKE_CXX_FLAGS="-Wall -rdynamic -finstrument-functions -fprofile-arcs -ftest-coverage -g"
         export LDFLAGS="-lgcov"
         export CMAKE_EXPORT_COMPILE_COMMANDS=ON
         cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_CXX_FLAGS="$CMAKE_CXX_FLAGS" -E env LDFLAGS="$LDFLAGS" ..
-	    VERBOSE=1 make
-	    #make install
-	elif [ -e *.pro ]
-	then
-	    qmake-qt5
-	    make
-	    #make install
-	elif [ -e configure ]
-	then
-	    ./configure
-	    make
-	    #make install
-	elif [ -e Makefile ]
-	then
-	    make
-	elif [ -e Makefile.am ]
-	then
-	    autoreconf -i
-	    automake
-	    autoconf
-	    ./configure
-	    make
-	    #make install
-	else
-	     echo "Unknown structure"
-	fi
+        VERBOSE=1 make
+        #make install
+    elif [ -e *.pro ]
+    then
+        qmake-qt5
+        make
+        #make install
+    elif [ -e configure ]
+    then
+        ./configure
+        make
+        #make install
+    elif [ -e Makefile ]
+    then
+        make
+    elif [ -e Makefile.am ]
+    then
+        autoreconf -i
+        automake
+        autoconf
+        ./configure
+        make
+        #make install
+    else
+         echo "Unknown structure"
+    fi
 else
-	$*
+    $*
 fi
