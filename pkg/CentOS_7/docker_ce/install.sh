ff28b215afe3b69e6fcd2a9f8f8dde11f6abf87f
Pablo <pablo.aledo@gmail.com>
2019-04-24 06:52:15 +0000

    Improve installer for docker_ce in centos7

--- pkg/CentOS_7/docker_ce/install.sh
+++ pkg/CentOS_7/docker_ce/install.sh
@@ -4,6 +4,6 @@ sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
-sudo yum install docker-ce
+sudo yum install -y docker-ce
 
-sudo systemctl start docker
+sudo systemctl enable docker
