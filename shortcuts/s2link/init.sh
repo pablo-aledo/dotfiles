259f26078c8e822fd0d625cc4162566a1e5371a7
Pablo <pablo.aledo@gmail.com>
2019-05-11 12:47:10 +0200

    fix s2link init

--- shortcuts/s2link/init.sh
+++ shortcuts/s2link/init.sh
@@ -1,8 +1,8 @@
 [ $# -eq 0 ] && [ -e /tmp/scount ] && count=$(cat /tmp/scount)
 [ $# -eq 0 ] && [ -e /tmp/query  ] && query=$(cat /tmp/query)
+[ $# -eq 0 ] && [ ! -e /tmp/scount ] && count=100
 
 [ $# -ge 1 ] && count=$1
-[ $# -ge 1 ] || count=100
 
 [ $# -ge 2 ] && query=$2
 [ $# -ge 2 ] || query="is: starred avxhm.se"
