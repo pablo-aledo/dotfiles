9cccbea0681ac80a008f986ecfd5170a5a955f85
Pablo <pablo.aledo@gmail.com>
2019-05-11 13:27:45 +0200

    fix s2link init

--- shortcuts/s2link/init.sh
+++ shortcuts/s2link/init.sh
@@ -1,14 +1,21 @@
 [ $# -eq 0 ] && [ -e /tmp/scount ] && count=$(cat /tmp/scount)
 [ $# -eq 0 ] && [ -e /tmp/query  ] && query=$(cat /tmp/query)
 [ $# -eq 0 ] && [ ! -e /tmp/scount ] && count=100
+[ $# -eq 0 ] && [ ! -e /tmp/s2query ] && query="is: starred avxhm.se"
+[ $# -eq 0 ] && skip=0
 
-[ $# -ge 1 ] && count=$1
 
-[ $# -ge 2 ] && query=$2
-[ $# -ge 2 ] || query="is: starred avxhm.se"
+[ $# -eq 1 ] && count=$1
+[ $# -eq 1 ] && query="is: starred avxhm.se"
+[ $# -eq 1 ] &&  skip=0
 
-[ $# -ge 3 ] && skip=$3
-[ $# -ge 3 ] || skip=0
+[ $# -eq 2 ] && count=$1
+[ $# -eq 2 ] && query=$2
+[ $# -eq 2 ] &&  skip=0
+
+[ $# -eq 3 ] && count=$1
+[ $# -eq 3 ] && query=$2
+[ $# -eq 3 ] &&  skip=$3
 
 echo $query > /tmp/query
 echo $count > /tmp/count
