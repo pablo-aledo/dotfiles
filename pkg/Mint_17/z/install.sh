f291afb70ddcc82c28615bedb2c1aa79cf4dec4a
Pablo <pablo.aledo@gmail.com>
2019-07-12 09:51:13 +0200

    Add fix to .z plugin

--- pkg/Mint_17/z/install.sh
+++ pkg/Mint_17/z/install.sh
@@ -2,3 +2,17 @@ wget https://raw.githubusercontent.com/rupa/z/master/z.sh -O ~/.z.sh
 echo 'source ~/.z.sh' >> ~/.shell 
 [ -d /media/DATA/z  ] && ( rm -fr ~/.z ; ln -s /media/DATA/z/.z ~/.z )
 [ -f /media/DATA/.z ] && ( rm -fr ~/.z ; ln -s /media/DATA/.z   ~/.z )
+
+# echo << EOF >> ~/.shell
+# if [ "$_Z_NO_RESOLVE_SYMLINKS" ]; then
+#     _z_precmd() {
+#         (_z --add "${PWD:a}" &)
+# 		: $RANDOM
+#     }
+# else
+#     _z_precmd() {
+#         (_z --add "${PWD:A}" &)
+# 		: $RANDOM
+#     }
+# fi
+# EOF
