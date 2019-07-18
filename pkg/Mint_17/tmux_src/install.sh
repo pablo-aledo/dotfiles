1303cc3f73dae6f98bbf8f3fc3abbafa313ffefc
Pablo <pablo.aledo@gmail.com>
2019-07-18 16:22:35 +0200

    add tmux installer from source

--- pkg/Mint_17/tmux_src/install.sh
+++ pkg/Mint_17/tmux_src/install.sh
@@ -1,12 +1,23 @@
+ROOTDIR=$OLDPWD
+
 wget https://github.com/libevent/libevent/releases/download/release-2.0.22-stable/libevent-2.0.22-stable.tar.gz
 tar xvf libevent-2.0.22-stable.tar.gz
 cd libevent-2.0.22-stable
-./configure --prefix=$HOME
+./configure --prefix=$ROOTDIR
 make # use make -j 8 to speed it up if your machine is capable
 make install
+cd ..
+
+wget https://ftp.gnu.org/gnu/ncurses/ncurses-5.7.tar.gz
+tar -xvzf ncurses-5.7.tar.gz
+cd ncureses-5.7
+./configure --prefix=$ROOTDIR
+make
+make install
+cd ..
 
 wget https://github.com/tmux/tmux/releases/download/2.2/tmux-2.2.tar.gz
 tar xvf tmux-2.2
-./configure --prefix=$HOME CFLAGS="-I$HOME/include" LDFLAGS="-L$HOME/lib"
+./configure --prefix=$ROOTDIR CFLAGS="-I$ROOTDIR/include" LDFLAGS="-L$ROOTDIR/lib"
 make
 make install
