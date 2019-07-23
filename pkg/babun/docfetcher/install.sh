33c30943ff17d97c85d9df2393349c72e3ebc9d3
Pablo <pablo.aledo@gmail.com>
2019-07-23 14:55:14 +0200

    docfetcher windows installer

---
+++ pkg/babun/docfetcher/install.sh
@@ -0,0 +1,6 @@
+cd /tmp
+wget https://downloads.sourceforge.net/project/docfetcher/docfetcher/1.1.22/docfetcher-1.1.22-portable.zip?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fdocfetcher%2Ffiles%2Fdocfetcher%2F1.1.22%2Fdocfetcher-1.1.22-portable.zip%2Fdownload&ts=1563886377 -O docfetcher.zip
+unzip docfetcher.zip
+mkdir -p /c/Users/$USER/Downloads/Programas/docfetcher
+cp DocFetcher-1.1.22/* /c/Users/$USER/Downloads/Programas/docfetcher
+
