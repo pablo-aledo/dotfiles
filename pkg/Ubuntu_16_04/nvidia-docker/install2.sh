5aee51376d07f9c6ba8797d60db91898f3f28f47
Pablo <pablo.aledo@gmail.com>
2019-08-06 08:54:19 +0000

    Add another nvidia-docker script

---
+++ pkg/Ubuntu_16_04/nvidia-docker/install2.sh
@@ -0,0 +1,10 @@
+# Install official NVIDIA driver package
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
+sudo apt-get update && sudo apt-get install -y --no-install-recommends linux-headers-generic dkms cuda-drivers
+
+# Install nvidia-docker and nvidia-docker-plugin
+wget -P /tmp https://github.com/NVIDIA/nvidia-docker/releases/download/v1.0.1/nvidia-docker_1.0.1-1_amd64.deb
+sudo dpkg -i /tmp/nvidia-docker*.deb && rm /tmp/nvidia-docker*.deb
+
+sudo nvidia-docker run --rm nvidia/cuda nvidia-smi
