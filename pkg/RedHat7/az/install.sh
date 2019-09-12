50f54e63feb376b26a4fc33d3175a7817e95b810
Pablo <pablo.aledo@gmail.com>
2019-09-12 12:45:47 +0000

    add az to RedHat7 installer

---
+++ pkg/RedHat7/az/install.sh
@@ -0,0 +1,3 @@
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
+sudo yum install azure-cli
