e1b2c48fbf68ecc6a2895210c8416dbd1fc68e86
Pablo <pablo.aledo@gmail.com>
2019-11-16 10:43:48 +0000

    udate cmake version in Ubuntu 16.04

--- pkg/Ubuntu_16_04/cmake/install.sh
+++ pkg/Ubuntu_16_04/cmake/install.sh
@@ -1,5 +1,5 @@
 cd
-wget https://cmake.org/files/v3.4/cmake-3.4.1-Linux-x86_64.tar.gz
-tar xf cmake-3.4.1-Linux-x86_64.tar.gz
-export PATH="$HOME/cmake-3.4.1-Linux-x86_64/bin:$PATH" # save it in .bashrc if needed
-echo 'export PATH="~/cmake-3.4.1-Linux-x86_64/bin:$PATH"' | sed "s|~|$HOME|g" >> ~/.paths
+wget https://cmake.org/files/v3.15/cmake-3.15.1-Linux-x86_64.tar.gz
+tar xf cmake-3.15.1-Linux-x86_64.tar.gz
+export PATH="$HOME/cmake-3.15.1-Linux-x86_64/bin:$PATH" # save it in .bashrc if needed
+echo 'export PATH="~/cmake-3.15.1-Linux-x86_64/bin:$PATH"' | sed "s|~|$HOME|g" >> ~/.paths
