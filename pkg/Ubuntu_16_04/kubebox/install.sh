d588228bbada6cbb31cddc4e8a155b9557ea1181
Pablo <pablo.aledo@gmail.com>
2019-11-22 14:23:12 +0000

    add kubebox

--- pkg/Ubuntu_16_04/kubebox/install.sh
+++ pkg/Ubuntu_16_04/kubebox/install.sh
@@ -1,3 +1,5 @@
-sudo wget https://github.com/astefanutti/kubebox/releases/download/v0.6.1/kubebox-linux -O /bin/kubebox
-chmod +x /bin/kubebox
+wget https://github.com/astefanutti/kubebox/releases/download/v0.6.1/kubebox-linux -O ~/kubebox
+chmod +x ~/kubebox
 
+# sudo wget https://github.com/astefanutti/kubebox/releases/download/v0.6.1/kubebox-linux -O /bin/kubebox
+# chmod +x /bin/kubebox
