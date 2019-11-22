99a4a7f1175114c79e7b97aaccb8f8b682ff5de2
Pablo <pablo.aledo@gmail.com>
2019-11-22 14:59:32 +0000

    kubectl tools

---
+++ pkg/Ubuntu_16_04/kube-prompt/install.sh
@@ -0,0 +1,5 @@
+cd /tmp
+wget https://github.com/c-bata/kube-prompt/releases/download/v1.0.9/kube-prompt_v1.0.9_linux_386.zip
+unzip kube-prompt_v1.0.9_linux_386.zip
+chmod +x kube-prompt
+sudo mv ./kube-prompt /usr/local/bin/kube-prompt
---
+++ pkg/Ubuntu_16_04/kubetail/install.sh
@@ -0,0 +1,3 @@
+cd
+wget https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail
+chmod +x kubetail
