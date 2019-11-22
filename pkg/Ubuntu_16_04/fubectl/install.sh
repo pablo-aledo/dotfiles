e70a39823ca42ddb684d71c9fd5a569c2543a62d
Pablo <pablo.aledo@gmail.com>
2019-11-22 13:50:55 +0000

    install fzf with fubectl

--- pkg/Ubuntu_16_04/fubectl/install.sh
+++ pkg/Ubuntu_16_04/fubectl/install.sh
@@ -1,3 +1,7 @@
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+printf 'y\ny\ny\n' | ~/.fzf/install
+source ~/.fzf.bash
+
 wget https://rawgit.com/kubermatic/fubectl/master/fubectl.source -O ~/.fubectl
 echo 'source ~/.fubectl' >> ~/.shell
 source ~/.fubectl
