ac91322dba1098de65c751ba340ac91cee018ee4
Pablo <pablo.aledo@gmail.com>
2019-12-07 10:50:43 +0000

    fix installation of levenshtein

--- pkg/Mint_17/levenshtein_pairs/install.sh
+++ pkg/Mint_17/levenshtein_pairs/install.sh
@@ -2,3 +2,5 @@ sudo cp ./levenshtein.pl /usr/bin
 sudo chmod +x /usr/bin/levenshtein.pl
 sudo mkdir -p /usr/local/lib64/perl5/Text
 sudo cp Levenshtein.pm /usr/local/lib64/perl5/Text
+sudo mkdir -p /etc/perl/Text
+sudo cp Levenshtein.pm /etc/perl/Text
