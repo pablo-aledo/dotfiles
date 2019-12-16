384b1c8a7b615762195093b74c8a336c19c73be4
Pablo <pablo.aledo@gmail.com>
2019-12-16 03:50:54 +0000

    Fix z asynchronous error

--- pkg/Ubuntu_16_04/z/install.sh
+++ pkg/Ubuntu_16_04/z/install.sh
@@ -3,16 +3,16 @@ echo 'source ~/.z.sh' >> ~/.shell
 [ -d /media/DATA/z  ] && ( rm -fr ~/.z ; ln -s /media/DATA/z/.z ~/.z )
 [ -f /media/DATA/.z ] && ( rm -fr ~/.z ; ln -s /media/DATA/.z   ~/.z )
 
-# echo << EOF >> ~/.shell
-# if [ "$_Z_NO_RESOLVE_SYMLINKS" ]; then
-#     _z_precmd() {
-#         (_z --add "${PWD:a}" &)
-# 		: $RANDOM
-#     }
-# else
-#     _z_precmd() {
-#         (_z --add "${PWD:A}" &)
-# 		: $RANDOM
-#     }
-# fi
-# EOF
+echo << EOF >> ~/.shell
+if [ "$_Z_NO_RESOLVE_SYMLINKS" ]; then
+    _z_precmd() {
+        (_z --add "${PWD:a}" &)
+		: $RANDOM
+    }
+else
+    _z_precmd() {
+        (_z --add "${PWD:A}" &)
+		: $RANDOM
+    }
+fi
+EOF
