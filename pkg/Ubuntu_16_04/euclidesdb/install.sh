9dad04d346673c7792ea17c63b39c891f32fda1f
Pablo <pablo.aledo@gmail.com>
2020-02-06 16:19:04 +0100

    add euclidesdb

---
+++ pkg/Ubuntu_16_04/euclidesdb/install.sh
@@ -0,0 +1,5 @@
+cd
+wget https://github.com/perone/euclidesdb/releases/download/v0.2.0/euclidesdb-0.2.0-Linux.tar.gz
+tar zxvf euclidesdb-0.2.0-Linux.tar.gz
+cd euclidesdb
+./euclidesdb -c euclidesdb.conf
