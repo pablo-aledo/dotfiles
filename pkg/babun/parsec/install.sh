ebfc8d4a7c7b38d10195d28d6e2fad03661d6dce
Pablo <pablo.aledo@gmail.com>
2020-02-13 11:18:43 +0100

    add installer for parsec and virtual_audio_cable

---
+++ pkg/babun/parsec/install.sh
@@ -0,0 +1,3 @@
+cd /tmp
+wget https://builds.parsecgaming.com/package/parsec-windows.exe
+cygstart parsec-windows.exe
---
+++ pkg/babun/virtual_audio_cable/install.sh
@@ -0,0 +1,3 @@
+cd /tmp
+wget https://download.vb-audio.com/Download_CABLE/VBCABLE_Driver_Pack43.zip
+unzip VBCABLE_Driver_Pack43.zip
