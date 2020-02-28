5be03a70681e41bc283cd4ec4622409bf9a336f3
Pablo <pablo.aledo@gmail.com>
2020-02-28 14:21:31 +0000

    noninteractive mutt install

--- pkg/Ubuntu_16_04/mutt/install.sh
+++ pkg/Ubuntu_16_04/mutt/install.sh
@@ -1,2 +1,4 @@
 export DEBIAN_FRONTEND=noninteractive
+echo "postfix postfix/mailname string example.com" | sudo debconf-set-selections
+echo "postfix postfix/main_mailer_type string 'Internet Site'" | sudo debconf-set-selections
 sudo apt-get -yq install mutt
