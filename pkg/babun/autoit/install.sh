94a585877258b69e2a27dbc76f957adaf072fb8d
Pablo <pablo.aledo@gmail.com>
2020-03-12 11:51:06 +0000

    autoit installer

---
+++ pkg/babun/autoit/install.sh
@@ -0,0 +1,7 @@
+cd /tmp
+
+wget https://www.autoitscript.com/cgi-bin/getfile.pl?autoit3/autoit-v3-setup.exe
+cygstart autoit-v3-setup.exe
+
+wget https://www.autoitscript.com/cgi-bin/getfile.pl?../autoit3/scite/download/SciTE4AutoIt3.exe
+cygstart SciTE4AutoIt3.exe
