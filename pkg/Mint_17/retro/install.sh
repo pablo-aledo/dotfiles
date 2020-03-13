b3955a72ef61b14e9dcc3570cde120745da22824
Pablo <pablo.aledo@gmail.com>
2020-03-13 05:19:46 +0000

    retro

---
+++ pkg/Mint_17/retro/install.sh
@@ -0,0 +1,13 @@
+sudo apt-get install -y visualboyadvance visualboyadvance-gtk
+sudo apt-get install -y dosbox
+sudo apt-get install -y dosemu
+sudo apt-get install -y desmume
+
+# ppsspp
+sudo add-apt-repository ppa:ppsspp/stable
+sudo apt-get update
+sudo apt-get install ppsspp
+
+# zsnes
+# gens-gs
+# dgen
