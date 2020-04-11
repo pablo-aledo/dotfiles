a2bbcbd980e648ed868692f3e9b395b4cb8f0697
Pablo <pablo.aledo@gmail.com>
2020-04-11 18:28:15 +0000

    windows and linux exploit suggesters

---
+++ pkg/Kali_rolling/les/install.sh
@@ -0,0 +1,2 @@
+cd
+git clone https://github.com/InteliSecureLabs/Linux_Exploit_Suggester.git
---
+++ pkg/Kali_rolling/wes/install.sh
@@ -0,0 +1,4 @@
+pip install xlrd --upgrade
+
+cd
+git clone https://github.com/AonCyberLabs/Windows-Exploit-Suggester.git
