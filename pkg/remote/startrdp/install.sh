e96e6ea0af9f4439d591ec8cae4baedee26f7727
Pablo <pablo.aledo@gmail.com>
2020-04-16 02:22:07 +0000

    Fix startrdp

--- pkg/remote/startrdp/install.sh
+++ pkg/remote/startrdp/install.sh
@@ -10,16 +10,26 @@ xdotool click 1; sleep 1
 xdotool mousemove 970 510; sleep 1
 xdotool click 1; sleep 1
 
-sleep 60
+ok=false
+while [ $ok = false ]
+do
+    mouseover 100 10000 rdpon.png; sleep 1
+    xdotool mousemove_relative 30 0; sleep 1
+    xdotool click 1; sleep 1
+    xdotool mousemove_relative 0 30; sleep 1
+    xdotool click 1; sleep 1
+    xdotool mousemove 820 560; sleep 1
+    xdotool click 1; sleep 1
+    mouseover 60 10000 copy.png error.png; sleep 1
+    if `mouseover 1 10000 error.png`
+    then
+        ok=false
+        xdotool mousemove 730 600; xdotool click 1; sleep 1
+    else
+        ok=true
+    fi
+done
 
-mouseover 100 10000 rdpon.png; sleep 1
-xdotool mousemove_relative 30 0; sleep 1
-xdotool click 1; sleep 1
-xdotool mousemove_relative 0 30; sleep 1
-xdotool click 1; sleep 1
-xdotool mousemove 820 560; sleep 1
-xdotool click 1; sleep 1
-mouseover 60 10000 copy.png; sleep 1
 xdotool click 1; sleep 1
 xdotool mousemove 860 510; sleep 1
 xdotool click 1; sleep 1
@@ -28,3 +38,5 @@ mouseover 10 10000 rdpon.png; sleep 1
 xdotool click 1; sleep 10
 xdotool key Ctrl+v; sleep 1
 xdotool key Return; sleep 1
+
+mouseover 10 10000 continue.png && xdotool click 1
