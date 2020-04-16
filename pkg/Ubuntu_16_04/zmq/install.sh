abf675f79ce35ff2552d712096069e60fc86f6c5
Pablo <pablo.aledo@gmail.com>
2020-04-16 04:16:33 +0000

    version 3 of zmq

--- pkg/Ubuntu_16_04/zmq/install.sh
+++ pkg/Ubuntu_16_04/zmq/install.sh
@@ -3,7 +3,7 @@ cd
 git clone --depth=1 https://github.com/imatix/zguide.git
 
 # C/C++
-sudo apt-get install -y pkg-config libzmq-dev libzmq3-dev
+sudo apt-get install -y pkg-config libzmq3-dev
 
 # Python
 sudo apt-get install -y python-pip
