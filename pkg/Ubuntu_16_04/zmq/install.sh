ab79ae6acd0b732c6688f35bbc9b4c60e956db69
Pablo <pablo.aledo@gmail.com>
2020-04-16 04:30:34 +0000

    zeromq for C

--- pkg/Ubuntu_16_04/zmq/install.sh
+++ pkg/Ubuntu_16_04/zmq/install.sh
@@ -2,8 +2,11 @@
 cd
 git clone --depth=1 https://github.com/imatix/zguide.git
 
-# C/C++
+# C++
 sudo apt-get install -y pkg-config libzmq3-dev
+
+# C
+sudo apt-get install -y libczmq-dev uuid-dev
 
 # Python
 sudo apt-get install -y python-pip
