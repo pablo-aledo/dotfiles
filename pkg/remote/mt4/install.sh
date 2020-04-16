c992ebca4c9794d45b534521668c67158a167234
Pablo <pablo.aledo@gmail.com>
2020-04-16 02:35:41 +0000

    icmarkets installer

---
+++ pkg/remote/mt4/install.sh
@@ -0,0 +1,11 @@
+mouseover 10 10000 chrome.png && xdotool click 1; xdotool click 1
+xdotool type https://download.mql5.com/cdn/web/13954/mt4/icmarketssc4setup.exe
+xdotool key Return
+
+mouseover 10 10000 icmarkets.png && xdotool click 1;
+
+xdotool key Alt+y
+
+xdotool key Return
+xdotool key Return
+
