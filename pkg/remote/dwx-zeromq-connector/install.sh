183b41741177dbfb5fc01b389b81549deda18eb7
Pablo <pablo.aledo@gmail.com>
2020-04-18 05:28:50 +0000

    dwx-zeromq-connector installer

---
+++ pkg/remote/dwx-zeromq-connector/install.sh
@@ -0,0 +1,23 @@
+mkd /tmp/dwx-zeromq-connector
+wget https://github.com/darwinex/dwx-zeromq-connector/archive/master.zip
+unzip master.zip
+unzip dwx-zeromq-connector-master/dependencies/mql-zmq-master.zip
+mkd c
+appid=1DAFD9A7C67DC84FE37EAA1FC1E5CF75
+
+mkdir -p Users/$USER/AppData/Roaming/MetaQuotes/Terminal/$appid/MQL4/Experts
+cp -r ../dwx-zeromq-connector-master/v2.0.2/mql4/DWX_ZeroMQ_Server_v2.0.2_RC1.mq4 Users/$USER/AppData/Roaming/MetaQuotes/Terminal/$appid/MQL4/Experts
+
+mkdir -p Users/$USER/AppData/Roaming/MetaQuotes/Terminal/$appid/MQL4/Include
+cp -r ../mql-zmq-master/Include/* Users/$USER/AppData/Roaming/MetaQuotes/Terminal/$appid/MQL4/Include
+
+mkdir -p Users/$USER/AppData/Roaming/MetaQuotes/Terminal/$appid/MQL4/Libraries
+cp -r ../mql-zmq-master/Library/MT4/* Users/$USER/AppData/Roaming/MetaQuotes/Terminal/$appid/MQL4/Libraries
+
+mkdir -p Users/$USER/AppData/Roaming/MetaQuotes/Terminal/$appid/MQL4/Scripts
+cp -r ../mql-zmq-master/Scripts/* Users/$USER/AppData/Roaming/MetaQuotes/Terminal/$appid/MQL4/Scripts
+
+zip -r mql-zmq-master.zip *
+
+mkdir .htaccess
+sudo docker run -d --rm -p 80:80 -v $PWD:/var/www/html fauria/lamp
