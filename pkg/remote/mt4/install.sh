79854d2372bc1a8437d765ac03538d77f27250bc
Pablo <pablo.aledo@gmail.com>
2020-04-18 10:45:36 +0000

    add times in icmarket

--- pkg/remote/mt4/install.sh
+++ pkg/remote/mt4/install.sh
@@ -1,11 +1,12 @@
-mouseover 10 10000 chrome.png && xdotool click 1; xdotool click 1
+mouseover 10 10000 chrome.png && xdotool click 1; xdotool click 1; sleep 10
+xdotool key Ctrl+l
 xdotool type https://download.mql5.com/cdn/web/13954/mt4/icmarketssc4setup.exe
 xdotool key Return
 
-mouseover 10 10000 icmarkets.png && xdotool click 1;
+mouseover 10 10000 icmarkets.png && xdotool click 1; sleep 5
 
-xdotool key Alt+y
+xdotool key Alt+y; sleep 10
 
-xdotool key Return
-xdotool key Return
+xdotool key space; sleep 15
+xdotool key space
 
