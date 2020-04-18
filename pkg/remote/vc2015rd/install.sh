a4ee4c8d82c16273806a2359a240cb91b385b5af
Pablo <pablo.aledo@gmail.com>
2020-04-18 11:19:58 +0000

    vc2015 redistributable

---
+++ pkg/remote/vc2015rd/install.sh
@@ -0,0 +1,18 @@
+mouseover 10 10000 chrome.png && xdotool click 1; xdotool click 1; sleep 10
+xdotool key Ctrl+l
+xdotool type 'https://www.microsoft.com/en-US/download/details.aspx?id=48145'
+xdotool key Return; sleep 5
+xdotool key space
+
+mouseover 10 10000 download.png && xdotool click 1; sleep 5
+
+xdotool mousemove 50 330; xdotool click 1
+
+mouseover 10 10000 next.png && xdotool click 1; sleep 5
+mouseover 10 10000 checkbox.png && xdotool click 1; sleep 5
+
+xdotool key Tab; sleep 1
+xdotool key space; sleep 1
+xdotool key Tab; sleep 1
+xdotool key space; sleep 1
+
