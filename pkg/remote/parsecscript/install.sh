77c241d405a4d6e59119005d1228a7691a1319d8
Pablo <pablo.aledo@gmail.com>
2020-04-19 10:19:46 +0000

    improve parsecscript installer

--- pkg/remote/parsecscript/install.sh
+++ pkg/remote/parsecscript/install.sh
@@ -1,12 +1,17 @@
 mouseover 10 10000 firefox.png && xdotool click 1; xdotool click 1; sleep 5
 xdotool key ctrl+l
 xdotool type 'https://raw.githubusercontent.com/pablo-aledo/dotfiles/master/pkg/babun/parsec/script.ps1'
+sleep 1
 xdotool key Return
 
-xdotool mousemove 500 500; xdotool click 3; sleep 1; xdotool key a
-xdotool mousemove 500 500; xdotool click 3; sleep 1; xdotool key c
+sleep 5
+
+xdotool mousemove 500 500; xdotool click 3; sleep 1; xdotool key a; sleep 1
+xdotool mousemove 500 500; xdotool click 3; sleep 1; xdotool key c; sleep 1
 
 mouseover 10 10000 win.png && xdotool click 1
 mouseover 10 10000 powershell.png && xdotool click 1
+
+sleep 1
 
 xdotool mousemove 500 500; xdotool click 3; xdotool key Return
