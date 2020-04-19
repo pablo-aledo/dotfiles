63580a512cfd7ca7491b19550ce96cea27851e58
Pablo <pablo.aledo@gmail.com>
2020-04-19 15:44:26 +0000

    fix backslash in read line

--- pkg/remote/parsecscript/install2.sh
+++ pkg/remote/parsecscript/install2.sh
@@ -2,7 +2,7 @@ mouseover 10 10000 win.png && xdotool click 1
 mouseover 10 10000 powershell.png && xdotool click 1
 sleep 1
 
-cat ../../babun/parsec/script.ps1 | while read line
+cat ../../babun/parsec/script.ps1 | while read -r line
 do
     xdotool type "$line"
     xdotool key Return
