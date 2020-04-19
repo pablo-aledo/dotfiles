6d53c03beab7adb10f6ea29fa983e3587f79a784
Pablo <pablo.aledo@gmail.com>
2020-04-19 14:02:58 +0000

    third way of installing parsecscript

--- pkg/remote/parsecscript/install3.sh
+++ pkg/remote/parsecscript/install3.sh
@@ -2,8 +2,6 @@ mouseover 10 10000 win.png && xdotool click 1
 mouseover 10 10000 powershell.png && xdotool click 1
 sleep 1
 
-
-
 xdotool type '[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"'; xdotool key Return; sleep 1
 xdotool type '(New-Object System.Net.WebClient).DownloadFile("https://github.com/jamesstringerparsec/Parsec-Cloud-Preparation-Tool/archive/master.zip","$ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool.zip")'; xdotool key Return; sleep 1
 xdotool type 'New-Item -Path $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool -ItemType Directory'; xdotool key Return; sleep 1
