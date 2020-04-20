d212d9e0b59c87dd708fd9647aa44e1b8fe02fbd
Pablo <pablo.aledo@gmail.com>
2020-04-20 18:26:48 +0000

    improve startparsec

--- pkg/remote/startparsec/install.sh
+++ pkg/remote/startparsec/install.sh
@@ -1,12 +1,5 @@
-mouseover 10 10000 parsec.png
-xdotool click 1
-xdotool click 1
-sleep 10
-
+mouseover 10 10000 parsec.png; xdotool click 1; xdotool click 1; sleep 10
 mouseover 10 10000 maximize.png && xdotool click 1;
-xdotool mousemove 800 500
-xdotool click 1
-xdotool click 5
+mouseover 10 10000 reload.png && xdotool click 1;
 mouseover 10 10000 share.png && xdotool click 1;
-
 mouseover 10 10000 closewin.png && xdotool click 1;
