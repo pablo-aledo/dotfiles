68561d41ddbdc4f4720170ccd9fea835b0af73de
Pablo <pablo.aledo@gmail.com>
2020-05-13 10:05:07 +0000

    add docker-squash

---
+++ pkg/Ubuntu_16_04/docker-squash/install.sh
@@ -0,0 +1,3 @@
+cd /tmp
+wget https://github.com/jwilder/docker-squash/releases/download/v0.2.0/docker-squash-linux-amd64-v0.2.0.tar.gz
+sudo tar -C /usr/local/bin -xzvf docker-squash-linux-amd64-v0.2.0.tar.gz
