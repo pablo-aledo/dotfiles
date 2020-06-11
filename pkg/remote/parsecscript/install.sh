298003212a379c34cef33caf57d5ffd9f1ab70d4
Pablo <pablo.aledo@gmail.com>
2020-06-11 03:40:55 +0000

    add delay in parsecscript typing

--- pkg/remote/parsecscript/install.sh
+++ pkg/remote/parsecscript/install.sh
@@ -3,8 +3,8 @@ mouseover 10 10000 powershell.png && xdotool click 1
 sleep 10
 
 xdotool type '[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"'; xdotool key Return; sleep 10
-xdotool type '(New-Object System.Net.WebClient).DownloadFile("https://github.com/jamesstringerparsec/Parsec-Cloud-Preparation-Tool/archive/master.zip","$ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool.zip")'; xdotool key Return; sleep 10
-xdotool type 'New-Item -Path $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool -ItemType Directory'; xdotool key Return; sleep 10
-xdotool type 'Expand-Archive $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool.Zip -DestinationPath $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool'; xdotool key Return; sleep 10
-xdotool type 'CD $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool\Parsec-Cloud-Preparation-Tool-master\'; xdotool key Return; sleep 10
-xdotool type 'Powershell.exe -File $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool\Parsec-Cloud-Preparation-Tool-master\Loader.ps1'; xdotool key Return; sleep 10
+xdotool type --delay 200 '(New-Object System.Net.WebClient).DownloadFile("https://github.com/jamesstringerparsec/Parsec-Cloud-Preparation-Tool/archive/master.zip","$ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool.zip")'; xdotool key Return; sleep 10
+xdotool type --delay 200 'New-Item -Path $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool -ItemType Directory'; xdotool key Return; sleep 10
+xdotool type --delay 200 'Expand-Archive $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool.Zip -DestinationPath $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool'; xdotool key Return; sleep 10
+xdotool type --delay 200 'CD $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool\Parsec-Cloud-Preparation-Tool-master\'; xdotool key Return; sleep 10
+xdotool type --delay 200 'Powershell.exe -File $ENV:UserProfile\Downloads\Parsec-Cloud-Preparation-Tool\Parsec-Cloud-Preparation-Tool-master\Loader.ps1'; xdotool key Return; sleep 10
