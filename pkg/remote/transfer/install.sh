372d90a80e2d7df9359fc048aa6c74e3e27ac89b
Pablo <pablo.aledo@gmail.com>
2020-06-15 19:11:42 +0000

    add transfer remote installer

---
+++ pkg/remote/transfer/install.sh
@@ -0,0 +1,14 @@
+mouseover 10 10000 win.png && xdotool click 1
+mouseover 10 10000 powershell.png && xdotool click 1
+sleep 10
+
+IP=$(myip | grep external | cut -d: -f2)
+
+cat /tmp/files | while read line
+do
+    FILENAME=$(basename $line)
+    xdotool type --delay 200 '(New-Object System.Net.WebClient).DownloadFile("https://'$IP'/'$FILENAME'","$ENV:UserProfile\Downloads\'$FILENAME'")'
+    xdotool key Return
+    sleep 10
+done
+
