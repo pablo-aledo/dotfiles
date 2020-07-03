18e610834dad7d845b10bf999b06fed83af31d04
Pablo <pablo.aledo@gmail.com>
2020-07-03 03:42:59 +0000

    improve nvidia_gaming

--- pkg/remote/nvidia_gaming/install.sh
+++ pkg/remote/nvidia_gaming/install.sh
@@ -3,18 +3,20 @@ xrandr --output VNC-0 --mode 1280x800
 
 google-chrome --profile-directory=cloud cloud.google.com >/dev/null 2>/dev/null &; sleep 10
 
-mouseover 20 10000 console.png && xdotool mousemove 0 0 && sleep 5
-mouseover 20 10000 console.png && xdotool click 1; sleep 1
+mouseover -1 10000 console.png && xdotool mousemove 0 0 && sleep 5
+mouseover -1 10000 console.png && xdotool click 1; sleep 1
 
-mouseover 20 10000 3bars.png && xdotool click 1; sleep 1
-mouseover 20 10000 market.png && xdotool click 1; sleep 1
-mouseover 20 10000 lens.png && xdotool mousemove_relative 50 0; xdotool click 1; sleep 1
+mouseover -1 10000 3bars.png && xdotool click 1; sleep 1
+mouseover -1 10000 market.png && xdotool click 1; sleep 1
+mouseover -1 10000 lens.png && xdotool mousemove_relative 50 0; xdotool click 1; sleep 1
 
 xdotool type "nvidia games"
 xdotool key Return
 
-mouseover 20 10000 windows.png && xdotool click 1; sleep 1
-mouseover 20 10000 iniciar.png && xdotool click 1; sleep 1
+mouseover -1 10000 windows.png && xdotool click 1; sleep 1
+mouseover -1 10000 iniciar.png && xdotool click 1; sleep 1
+
+sleep 10
 
 xdotool key Tab
 xdotool key Tab
@@ -22,26 +24,26 @@ xdotool key Tab
 xdotool key Tab
 xdotool key Down
 
-mouseover 20 1000 euwest.png && xdotool click 1; sleep 1
+mouseover -1 1000 euwest.png && xdotool click 1; sleep 1
 
 for a in $(seq 1 10)
 do
 xdotool click 5
 done
 
-mouseover 20 1000 desplegar.png && xdotool click 1; sleep 1
+mouseover -1 1000 desplegar.png && xdotool click 1; sleep 1
 
-mouseover 20 1000 ok.png
+mouseover -1 1000 ok.png
 
-mouseover 20 10000 3bars.png && xdotool click 1; sleep 1
-mouseover 20 10000 computeengine.png && xdotool click 1; sleep 1
+mouseover -1 10000 3bars.png && xdotool click 1; sleep 1
+mouseover -1 10000 computeengine.png && xdotool click 1; sleep 1
 
-mouseover 20 10000 rdpon.png; sleep 30
+mouseover -1 10000 rdpon.png; sleep 30
 
 ok=false
 while [ $ok = false ]
 do
-    mouseover 20 10000 rdpon.png; sleep 1
+    mouseover -1 10000 rdpon.png; sleep 1
     xdotool mousemove_relative 30 0; sleep 1
     xdotool click 1; sleep 1
     xdotool mousemove_relative 0 30; sleep 1
@@ -62,7 +64,7 @@ xdotool click 1; sleep 1
 xdotool mousemove 860 510; sleep 1
 xdotool click 1; sleep 1
 
-mouseover 20 10000 rdpon.png; sleep 1
+mouseover -1 10000 rdpon.png; sleep 1
 xdotool click 1; sleep 10
 xdotool key Ctrl+v; sleep 1
 xdotool key Return; sleep 1
