6ad15385b6489a377f01676487da68961e20d6a5
Pablo <pablo.aledo@gmail.com>
2020-07-11 03:57:01 +0000

    improve steam_powershell remote installer

--- pkg/remote/steam_powershell/install.sh
+++ pkg/remote/steam_powershell/install.sh
@@ -3,5 +3,17 @@ mouseover 10 10000 powershell.png && xdotool click 1
 mouseover 10 10000 maximize.png && xdotool click 1
 sleep 10
 
-xdotool type --delay 200 '(New-Object System.Net.WebClient).DownloadFile("https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe","$ENV:UserProfile\Downloads\SteamSetup.exe")'
-xdotool type --delay 200 '$ENV:UserProfile\Downloads\SteamSetup.exe'
+xdotool type --delay 200 '(New-Object System.Net.WebClient).DownloadFile("https://steamcdn-a.akamaihd.net/client/installer/SteamSetup.exe","$ENV:UserProfile\Downloads\SteamSetup.exe")'; xdotool key Return; sleep 10
+xdotool type --delay 200 'Invoke-Item $ENV:UserProfile\Downloads\SteamSetup.exe'; xdotool key Return; sleep 10
+
+mouseover -1 10000 yes.png && xdotool click 1
+mouseover -1 10000 next.png && xdotool click 1
+mouseover -1 10000 next.png && xdotool click 1
+mouseover -1 10000 install.png && xdotool click 1
+mouseover -1 10000 checkbox.png && xdotool click 1
+mouseover -1 10000 finish.png && xdotool click 1
+
+for a in $(seq 1 10)
+do
+    mouseover 1 10000 closewin1.png closewin2.png && xdotool click 1
+done
