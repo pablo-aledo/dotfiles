e840961687a15e4b658772c587e699a0e5fafc2d
Pablo <pablo.aledo@gmail.com>
2020-07-21 03:25:49 +0000

    improve parsecscript

--- pkg/remote/parsecscript/install.sh
+++ pkg/remote/parsecscript/install.sh
@@ -12,7 +12,7 @@ xdotool type --delay 200 'Powershell.exe -File $ENV:UserProfile\Downloads\Parsec
 
 mouseover -1 10000 yes.png && xdotool click 1
 mouseover -1 10000 yn.png && xdotool key n; xdotool key Return
-mouseover -1 10000 yn.png && xdotool key n; xdotool key Return
+mouseover -1 10000 tesla.png
 mouseover -1 10000 yn.png && xdotool key n; xdotool key Return
 mouseover -1 10000 enter.png && xdotool click 1 && xdotool key Return
 mouseover -1 10000 closewin3.png && xdotool click 1
