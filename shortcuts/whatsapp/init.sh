be8d45232b0da5dd591f7fcc455cae3a922d24fd
Pablo <pablo.aledo@gmail.com>
2020-10-07 07:28:57 +0000

    change whatsapp

--- shortcuts/whatsapp/init.sh
+++ shortcuts/whatsapp/init.sh
@@ -5,3 +5,11 @@ for a in $*
 do
     echo $a | sed "s|^|$PWD/|g"
 done > /tmp/whatsapp_files
+
+export DISPLAY=:1
+source ~/.dotfiles/source/.shell/adapt
+xdotool key 1; shcw 1
+xdotool key 2; shcw 2
+xdotool key 3; shcw 3
+xdotool key 4; shcw 4
+xdotool key 0
