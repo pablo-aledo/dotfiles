635367b71b1c9162fb340473c82184b2432fba75
Pablo <pablo.aledo@gmail.com>
2020-10-25 21:02:40 +0000

    add midi2ascii installer

--- pkg/Mint_17/midi2ascii/install.sh
+++ pkg/Mint_17/midi2ascii/install.sh
@@ -2,5 +2,6 @@ mkdir ~/midi2ascii
 cd ~/midi2ascii
 wget http://www.archduke.org/midi/asc2mid.c
 wget http://www.archduke.org/midi/mid2asc.c
+wget http://www.archduke.org/midi/instrux.html
 gcc asc2mid.c -o asc2mid
 gcc mid2asc.c -lm -o mid2asc
