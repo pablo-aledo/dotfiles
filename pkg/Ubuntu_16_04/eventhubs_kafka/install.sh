c43d37fe78dadd52537ca61d26bc6455feadf7aa
Pablo <pablo.aledo@gmail.com>
2020-10-26 11:14:56 +0000

    update eventhubs to compile quickstart

--- pkg/Ubuntu_16_04/eventhubs_kafka/install.sh
+++ pkg/Ubuntu_16_04/eventhubs_kafka/install.sh
@@ -1,11 +1,11 @@
 cd; mkdir aeh; cd aeh
 wget https://github.com/Azure/azure-event-hubs-for-kafka/archive/master.zip
 unzip master.zip
-cd azure-event-hubs-for-kafka-master/tutorials/akka/java/producer
+cd azure-event-hubs-for-kafka-master/quickstart/java/producer
 pkg install maven default-jdk
-vim src/main/resources/application.conf
+vim src/main/resources/producer.config
 mvn clean package
-mvn -e exec:java -Dexec.mainClass="AkkaTestProducer"
+mvn -e exec:java -Dexec.mainClass="TestProducer"
 
 cp ~/.dotfiles/pkg/Ubuntu_16_04/eventhubs_kafka/pom.xml pom.xml
 mvn clean compile assembly:single
