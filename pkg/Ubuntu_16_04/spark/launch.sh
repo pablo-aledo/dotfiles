c908a08fd8c521b636ffda54e88995b2bd54a39c
Pablo <pablo.aledo@gmail.com>
2020-11-16 16:10:22 +0000

    add spark-launch example

--- pkg/Ubuntu_16_04/spark/launch.sh
+++ pkg/Ubuntu_16_04/spark/launch.sh
@@ -62,3 +62,26 @@ spark-submit \
   --num-executors 50 \
   http://path/to/examples.jar \
   1000
+
+# Run on a Kubernetes cluster in cluster deploy mode
+
+k create serviceaccount spark
+k policy add-role-to-user admin system:serviceaccount:project:spark
+k create clusterrolebinding spark-role --clusterrole=edit --serviceaccount=project:spark --namespace=project
+
+spark-submit \
+  --class org.apache.spark.examples.SparkPi \
+  --master k8s://https://xx.yy.zz.ww:443 \
+  --deploy-mode cluster \
+  --name sparkpi \
+  --conf spark.executor.instances=1 \
+  --conf spark.kubernetes.container.image=registry... \
+  --conf spark.kubernetes.authenticate.driver.serviceAccountName=spark \
+  --conf spark.kubernetes.authenticate.submission.oauthToken=$(kubectl sa get-token spark) \
+  --conf spark.kubernetes.namespace=project \
+  --conf spark.kubernetes.timeout=100 \
+  --executor-memory 20G \
+  --num-executors 50 \
+  http://path/to/spark-examples_2.11-2.4.5.jar \
+  1000
+
