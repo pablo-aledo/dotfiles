53241d0c108611a80bf3dd2afca7a4b752cd6e6d
Pablo <pablo.aledo@gmail.com>
2020-12-07 17:05:03 +0000

    ffmpeg-concat create npm directory

--- pkg/Ubuntu_18_04/ffmpeg-concat-2/install.sh
+++ pkg/Ubuntu_18_04/ffmpeg-concat-2/install.sh
@@ -1,3 +1,4 @@
 sudo apt-get install -y ffmpeg npm xvfb
-sudo npm install -g ffmpeg-concat
+sudo mkdir -p /root/.cache/node-gyp/
+sudo npm install --unsafe-perm -g ffmpeg-concat
 # xvfb-run -s "-ac -screen 0 1280x1024x24" ffmpeg-concat --frame-format jpg -o concat.mp4 input_1.mp4 input_2.mp4
