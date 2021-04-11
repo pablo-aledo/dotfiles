cc0fd38f55e93889143f06b97a4ea766815f6812
Pablo <pablo.aledo@gmail.com>
2021-04-11 06:53:59 +0000

    update startrdp

--- pkg/remote/startrdp/install.sh
+++ pkg/remote/startrdp/install.sh
@@ -3,34 +3,34 @@ xrandr --output VNC-0 --mode 1280x800
 
 google-chrome --profile-directory=cloud cloud.google.com >/dev/null 2>/dev/null &; sleep 10
 
-mouseover 20 10000 console.png && xdotool mousemove 0 0 && sleep 5
-mouseover 20 10000 console.png && xdotool click 1; sleep 1
+mouseover -1 10000 console.png && xdotool mousemove 0 0 && sleep 5
+mouseover -1 10000 console.png && xdotool click 1; sleep 1
 
-mouseover 20 10000 computeengine.png && xdotool mousemove 0 0 && sleep 1
-mouseover 20 10000 computeengine.png && xdotool click 1; sleep 1
+mouseover -1 10000 computeengine.png && xdotool mousemove 0 0 && sleep 1
+mouseover -1 10000 computeengine.png && xdotool click 1; sleep 1
 
-mouseover 20 10000 rdpoff.png && xdotool mousemove 0 0 && sleep 1
-mouseover 20 10000 rdpoff.png; sleep 1
-xdotool mousemove_relative 70 0; sleep 1
+mouseover -1 10000 rdpoff.png && xdotool mousemove 0 0 && sleep 1
+mouseover -1 10000 rdpoff.png; sleep 1
+xdotool mousemove_relative 80 0; sleep 1
 xdotool click 1; sleep 1
 xdotool mousemove_relative 0 30; sleep 1
 xdotool click 1; sleep 1
-xdotool mousemove 970 510; sleep 1
+mouseover -1 10000 start.png; sleep 1
 xdotool click 1; sleep 1
 
-mouseover 20 10000 rdpon.png; sleep 30
+mouseover -1 10000 rdpon.png; sleep 30
 
 ok=false
 while [ $ok = false ]
 do
-    mouseover 20 10000 rdpon.png; sleep 1
+    mouseover -1 10000 rdpon.png; sleep 1
     xdotool mousemove_relative 30 0; sleep 1
     xdotool click 1; sleep 1
     xdotool mousemove_relative 0 30; sleep 1
     xdotool click 1; sleep 1
-    xdotool mousemove 860 560; sleep 1
+    mouseover -1 10000 set.png; sleep 1
     xdotool click 1; sleep 1
-    mouseover 20 10000 copy.png error.png; sleep 1
+    mouseover -1 10000 copy.png error.png; sleep 1
     if `mouseover 1 10000 error.png`
     then
         ok=false
@@ -41,10 +41,10 @@ do
 done
 
 xdotool click 1; sleep 1
-xdotool mousemove 860 510; sleep 1
+mouseover -1 10000 close.png; sleep 1
 xdotool click 1; sleep 1
 
-mouseover 20 10000 rdpon.png; sleep 1
+mouseover -1 10000 rdpon.png; sleep 1
 xdotool click 1; sleep 10
 xdotool key Ctrl+v; sleep 1
 xdotool key Return; sleep 1
