c34eee948b3e4ae9a9abb6679d33d882699e05ea
Pablo <pablo.aledo@gmail.com>
2021-04-12 05:24:59 +0000

    update startrdp

--- pkg/remote/startrdp/install.sh
+++ pkg/remote/startrdp/install.sh
@@ -1,13 +1,13 @@
 export DISPLAY=:1
 xrandr --output VNC-0 --mode 1280x800
 
-google-chrome --profile-directory=cloud cloud.google.com >/dev/null 2>/dev/null &; sleep 10
+google-chrome --profile-directory=cloud console.cloud.google.com/compute/instances >/dev/null 2>/dev/null &; sleep 10
 
-mouseover -1 10000 console.png && xdotool mousemove 0 0 && sleep 5
-mouseover -1 10000 console.png && xdotool click 1; sleep 1
+#mouseover -1 10000 console.png && xdotool mousemove 0 0 && sleep 5
+#mouseover -1 10000 console.png && xdotool click 1; sleep 1
 
-mouseover -1 10000 computeengine.png && xdotool mousemove 0 0 && sleep 1
-mouseover -1 10000 computeengine.png && xdotool click 1; sleep 1
+#mouseover -1 10000 computeengine.png && xdotool mousemove 0 0 && sleep 1
+#mouseover -1 10000 computeengine.png && xdotool click 1; sleep 1
 
 mouseover -1 10000 rdpoff.png && xdotool mousemove 0 0 && sleep 1
 mouseover -1 10000 rdpoff.png; sleep 1
