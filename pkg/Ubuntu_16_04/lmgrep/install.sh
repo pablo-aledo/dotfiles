f38dfa651cfff82efa79352d72018d8184abde20
Pablo <pablo.aledo@gmail.com>
2021-04-25 14:50:20 +0000

    lmgrep

---
+++ pkg/Ubuntu_16_04/lmgrep/install.sh
@@ -0,0 +1,4 @@
+cd /tmp
+wget https://github.com/dainiusjocas/lucene-grep/releases/download/v2021.04.23/lmgrep-v2021.04.23-linux-static-amd64.zip
+unzip lmgrep-v2021.04.23-linux-static-amd64.zip
+sudo cp lmgrep /bin
