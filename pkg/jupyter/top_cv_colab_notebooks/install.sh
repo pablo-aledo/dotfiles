28ca0b7d36585c3adb6aa3f526c548ff05b1053a
Pablo <pablo.aledo@gmail.com>
2021-07-08 07:54:08 +0000

    fix top_cv_colab_notebooks

--- pkg/jupyter/top_cv_colab_notebooks/install.sh
+++ pkg/jupyter/top_cv_colab_notebooks/install.sh
@@ -1,1 +1,1 @@
-wget https://www.qblocks.cloud/creators/computer-vision-google-colab-notebooks -O $OLDPWD/computer-vision-google-colab-notebooks
+wget --no-check-certificate https://www.qblocks.cloud/creators/computer-vision-google-colab-notebooks -O $OLDPWD/computer-vision-google-colab-notebooks
