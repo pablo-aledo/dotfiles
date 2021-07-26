cc028f443bf341586289d5a24fdc15d78dd88a71
Pablo <pablo.aledo@gmail.com>
2021-07-26 11:26:31 +0000

    fix firewall_gcp

--- pkg/remote/firewall_gcp/install.sh
+++ pkg/remote/firewall_gcp/install.sh
@@ -1,9 +1,3 @@
-    echo moonligt > /tmp/name
-    echo 47984, 47989, 48010 > /tmp/tcp
-    echo 5353, 47998, 47999, 48000, 48002, 48010 > /tmp/udp
-
-
-
 google-chrome --profile-directory=cloud console.cloud.google.com/networking/firewalls/ >/dev/null 2>/dev/null & sleep 10
 
 mouseover -1 10000 plus.png && xdotool click 1; sleep 1
