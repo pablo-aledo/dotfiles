1025f8a955024767843983066dde0aefa8bbae74
Pablo <pablo.aledo@gmail.com>
2021-12-02 09:44:02 +0000

    Prepare for Arch

--- pkg/Arch/yay/install.sh
+++ pkg/Arch/yay/install.sh
@@ -1,6 +1,9 @@
 sudo pacman -S --noconfirm libffi
 sudo pacman -S --noconfirm fakeroot
 sudo pacman -S --noconfirm make
+sudo pacman -S --noconfirm binutils
+sudo pacman -S --noconfirm gcc
+sudo pacman -S --noconfirm pacman
 cd /tmp
 git clone https://aur.archlinux.org/yay.git
 cd yay
