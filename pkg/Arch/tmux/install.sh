f65c81d8d426f2b0e5865ca92c58bbe6d1c22971
Pablo <pablo.aledo@gmail.com>
2021-12-02 10:57:00 +0000

    tmux installer for arch

---
+++ pkg/Arch/tmux/install.sh
@@ -0,0 +1,14 @@
+sudo pcmanfm -S --noconfirm libevent ncurses
+
+VERSION=2.1
+sudo mkdir /usr/src/tmux; sudo chmod 777 /usr/src/tmux; cd /usr/src/tmux
+wget https://github.com/tmux/tmux/releases/download/$VERSION/tmux-$VERSION.tar.gz
+tar xf tmux-$VERSION.tar.gz
+rm -f tmux-$VERSION.tar.gz
+cd tmux-$VERSION
+./configure
+make
+sudo make install
+cd -
+sudo rm -rf /usr/local/src/tmux-*
+sudo mv tmux-$VERSION /usr/local/src
