3b8deb4420f231a8777cd4cfa5fb0fb124402e31
Pablo <pablo.aledo@gmail.com>
2021-12-05 06:11:26 +0000

    fix rsstail installer

--- pkg/Arch/rsstail/install.sh
+++ pkg/Arch/rsstail/install.sh
@@ -1,4 +1,15 @@
-sudo pacman -S --noconfirm pkgconf
+sudo pacman -S --noconfirm pkgconf fakeroot
+
+cd /tmp
+git clone https://aur.archlinux.org/libnxml.git
+cd libnxml
+makepkg -si --noconfirm
+
+cd /tmp
+git clone https://aur.archlinux.org/libmrss.git
+cd libmrss
+makepkg -si --noconfirm
+
 cd /tmp
 git clone https://aur.archlinux.org/rsstail.git
 cd rsstail
