35f8ba384c68c6032aa9716673d2b249cd3aef48
Pablo <pablo.aledo@gmail.com>
2021-12-14 09:41:59 +0000

    add csharp to rtow

--- pkg/Ubuntu_16_04/rtow/install.sh
+++ pkg/Ubuntu_16_04/rtow/install.sh
@@ -1,4 +1,4 @@
-for lang in c c++ rust go scala cuda opencl crystal nim javascript haskell java julia lua v python
+for lang in c c++ rust go scala cuda opencl crystal nim javascript haskell java julia lua v python csharp
 do
     mkdir $lang
     cd $lang
