e3ca53583cbbdb72f80774fafbb6f306e1b917e1
Pablo <pablo.aledo@gmail.com>
2021-12-16 22:00:20 +0000

    add repos to rtow

--- pkg/Ubuntu_16_04/rtow/install.sh
+++ pkg/Ubuntu_16_04/rtow/install.sh
@@ -5,7 +5,7 @@ do
     #googler "raytracer in one weekend $lang site: github.com" --json | grep '"url":' | cut -d'"' -f4 > urls
     cat urls | head -n99 | while read url
     do
-        zipurl="$url/archive/refs/heads/master.zip"
+        zipurl="$url/archive/refs/heads/main.zip"
         name="$(echo $url | cut -d/ -f4).$(echo $url | cut -d/ -f5).zip"
         [ -e $name ] || wget $zipurl -O $name
     done
