a646b81622f0cbf6b3a39ef0acb3ff13dda3a2b4
Pablo <pablo.aledo@gmail.com>
2022-03-18 09:01:21 +0000

    add cmake-init

---
+++ pkg/Arch/cmake-init/install.sh
@@ -0,0 +1,11 @@
+pkg install python
+pkg install cmake
+pkg install git
+# pkg install clang-tidy
+pkg install cppcheck
+pkg install doxygen
+pkg install lcov
+# pkg install clang-format
+pkg install codespell
+pkg install conan
+pip install cmake-init
