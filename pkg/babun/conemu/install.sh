4b6989327c3a4fcbc8d5d61249df1ac3be164a50
pablo-aledo <pablo.aledo@gmail.com>
2022-03-29 14:52:31 +0200

    add conemu

---
+++ pkg/babun/conemu/install.sh
@@ -0,0 +1,7 @@
+cd /tmp
+mkdir conemu
+cd conemu
+wget https://downloads.sourceforge.net/project/conemu/Preview/ConEmuPack.180528.7z
+unzip ConEmuPack.180528.7z
+mkdir -p /c/Users/$USER/Downloads/Programas/conemu
+cp * /c/Users/$USER/Downloads/Programas/conemu
