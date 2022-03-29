e34c1d1198d8446bc5dfd4817ae296976875847c
pablo-aledo <pablo.aledo@gmail.com>
2022-03-29 14:57:33 +0200

    add tabby

---
+++ pkg/babun/tabby/install.sh
@@ -0,0 +1,7 @@
+cd /tmp
+mkdir tabby
+cd tabby
+wget https://github.com/Eugeny/tabby/releases/download/v1.0.174/tabby-1.0.174-portable.zip
+unzip tabby-1.0.174-portable.zip
+mkdir -p /c/Users/$USER/Downloads/Programas/tabby
+cp * /c/Users/$USER/Downloads/Programas/tabby
