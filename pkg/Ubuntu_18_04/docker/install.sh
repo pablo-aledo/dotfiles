e6d5f107a495bc919fd196a96171cb38eb531a2d
Pablo <pablo.aledo@gmail.com>
2022-07-01 04:26:16 +0000

    docker for ubuntu 18

---
+++ pkg/Ubuntu_18_04/docker/install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install curl apt-transport-https ca-certificates software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+apt-cache policy docker-ce
+sudo apt install docker-ce
