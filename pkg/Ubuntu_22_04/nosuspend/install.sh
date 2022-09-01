a2012b479d357dd636bef547806880bbdf310aae
Pablo <pablo.aledo@gmail.com>
2022-09-01 12:54:01 +0000

    add nosuspend and xorg

---
+++ pkg/Ubuntu_22_04/nosuspend/install.sh
@@ -0,0 +1,1 @@
+sudo sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=ignore/g' /etc/systemd/logind.conf
---
+++ pkg/Ubuntu_22_04/xorg/install.sh
@@ -0,0 +1,1 @@
+sudo sed -i 's/#WaylandEnable=false/WaylandEnable=false/g' /etc/gdm3/custom.conf
