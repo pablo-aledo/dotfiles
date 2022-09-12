7a82e489f8d66880fe206cf862df5aec463fd984
Pablo <pablo.aledo@gmail.com>
2022-09-12 12:14:13 +0000

    Fix chrome freeze open

--- pkg/Ubuntu_22_04/google-chrome/install.sh
+++ pkg/Ubuntu_22_04/google-chrome/install.sh
@@ -3,3 +3,4 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb 
 sudo apt-get -f install -y
 
+sudo apt install -f xdg-desktop-portal-gnome
