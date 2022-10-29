9d2dbc50f3a46588db60558c82d58a4efc117242
pablo-aledo <pablo.aledo@gmail.com>
2022-10-29 04:55:38 +0200

    yuzu and ryujinx

---
+++ pkg/babun/ryujinx/install.sh
@@ -0,0 +1,3 @@
+cd /tmp
+wget https://github.com/Ryujinx/release-channel-master/releases/download/1.1.330/ryujinx-1.1.330-win_x64.zip
+unzip ryujinx-1.1.330-win_x64.zip
---
+++ pkg/babun/yuzu/install.sh
@@ -0,0 +1,3 @@
+cd /tmp
+wget https://github.com/yuzu-emu/yuzu-mainline/releases/download/mainline-0-1213/yuzu-windows-msvc-20221028-d5e4359db.zip
+unzip yuzu-windows-msvc-20221028-d5e4359db.zip
