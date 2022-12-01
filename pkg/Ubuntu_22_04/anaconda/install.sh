e098007dd856ebdc5c0e9aa298b00f9cf2079055
Pablo <pablo.aledo@gmail.com>
2022-12-01 10:20:02 +0000

    unattended conda

--- pkg/Ubuntu_22_04/anaconda/install.sh
+++ pkg/Ubuntu_22_04/anaconda/install.sh
@@ -1,6 +1,6 @@
 cd
 curl -O https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-$(arch).sh
-bash ./Anaconda3-2022.10-Linux-$(arch).sh
+bash ./Anaconda3-2022.10-Linux-$(arch).sh -b -p $HOME/anaconda3
 echo 'path ~/anaconda3/bin' >> ~/.paths
 source ~/.paths
 
