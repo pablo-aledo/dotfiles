193e36aaf1085f477d26d3ec64017af46de62422
Pablo <pablo.aledo@gmail.com>
2022-12-12 05:41:11 +0000

    update obsidian version

--- pkg/Ubuntu_16_04/obsidian/install.sh
+++ pkg/Ubuntu_16_04/obsidian/install.sh
@@ -1,2 +1,2 @@
-sudo wget https://github.com/obsidianmd/obsidian-releases/releases/download/v0.11.13/Obsidian-0.11.13.AppImage -O /bin/obsidian
+sudo wget https://github.com/obsidianmd/obsidian-releases/releases/download/v1.0.3/Obsidian-1.0.3.AppImage -O /bin/obsidian
 sudo chmod +x /bin/obsidian
