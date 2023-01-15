4cd82bab8d09e6901539830c32df9db3aaaa02ce
Pablo <pablo.aledo@gmail.com>
2023-01-15 19:16:19 +0000

    add stable_diffusion_ui

---
+++ pkg/Arch/stable_diffusion_ui/install.sh
@@ -0,0 +1,5 @@
+cd
+wget https://github.com/cmdr2/stable-diffusion-ui/releases/download/v2.4.13/stable-diffusion-ui-linux.zip
+unzip stable-diffusion-ui-linux.zip
+cd stable-diffusion-ui
+./start.sh
