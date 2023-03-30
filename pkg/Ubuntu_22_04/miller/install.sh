e712a685b6c8dd744a4c95412a2df2c25e4b84fe
Pablo <pablo.aledo@gmail.com>
2023-03-30 11:21:19 +0000

    add miller

---
+++ pkg/Ubuntu_22_04/miller/install.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -f miller
+# cd /tmp
+# wget https://github.com/johnkerl/miller/releases/download/v6.7.0/miller-6.7.0-linux-amd64.tar.gz
+# tar -xvzf miller-6.7.0-linux-amd64.tar.gz
+# sudo cp miller-6.7.0-linux-amd64/mlr /usr/bin
