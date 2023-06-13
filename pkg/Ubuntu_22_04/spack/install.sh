c2bc7a34e6eb154504741dffd137537be9a33f34
Pablo <pablo.aledo@gmail.com>
2023-06-13 12:59:07 +0000

    add spack

---
+++ pkg/Ubuntu_22_04/spack/install.sh
@@ -0,0 +1,16 @@
+sudo apt update
+sudo apt install -y build-essential ca-certificates coreutils curl environment-modules gfortran git gpg lsb-release python3 python3-distutils python3-venv unzip zip
+
+cd
+git clone -c feature.manyFiles=true https://github.com/spack/spack.git
+cd spack
+git checkout v0.20.0
+. share/spack/setup-env.sh
+echo '. ~/spack/share/spack/setup-env.sh' >> ~/.paths
+
+# cd
+# mkd spack_yaml
+# spack env activate .
+# spack concretize -f
+# spack install
+
