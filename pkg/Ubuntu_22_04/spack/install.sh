b50864f18d40769ac70012e87ceaf9f31df4d9bd
Pablo <pablo.aledo@gmail.com>
2023-06-13 14:59:59 +0000

    add g++-12 to spack

--- pkg/Ubuntu_22_04/spack/install.sh
+++ pkg/Ubuntu_22_04/spack/install.sh
@@ -1,4 +1,7 @@
 sudo apt update
+
+sudo apt-get install g++-12
+
 sudo apt install -y build-essential ca-certificates coreutils curl environment-modules gfortran git gpg lsb-release python3 python3-distutils python3-venv unzip zip
 
 cd
