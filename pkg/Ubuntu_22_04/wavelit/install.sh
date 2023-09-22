e30df808b6e061c3347334fe18be67952a138bfc
Pablo <pablo.aledo@gmail.com>
2023-09-22 14:54:23 +0000

    add -w to Wavelit

--- pkg/Ubuntu_22_04/wavelit/install.sh
+++ pkg/Ubuntu_22_04/wavelit/install.sh
@@ -2,5 +2,5 @@ sudo wget https://github.com/seqeralabs/wavelit/releases/download/v0.7.0/wavelit
 sudo chmod +x /usr/bin/wavelit
 
 Wavelit(){
-    docker run --privileged  -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -it $(wavelit --config-file https://fusionfs.seqera.io/releases/v2.2.7-amd64.json --conda-package="conda-forge::procps-ng $1")
+    docker run --privileged -w $2 -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -it $(wavelit --config-file https://fusionfs.seqera.io/releases/v2.2.7-amd64.json --conda-package="conda-forge::procps-ng $1")
 }
