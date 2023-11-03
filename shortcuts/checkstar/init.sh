709e74674bb2289bafdd8dd7d8ee9982a4294a15
Pablo <pablo.aledo@gmail.com>
2023-11-03 08:59:01 +0000

    rm checkstar

--- shortcuts/checkstar/init.sh
+++ shortcuts/checkstar/init.sh
@@ -3,3 +3,11 @@
 
 echo $filter > /tmp/checkstar_filter
 [ $file != /tmp/titles ] && \cp $file /tmp/titles
+
+Rm () {
+        [ "$1" = "-rf" ] && dir=~/.Trash/`date +%s`  && mkdir -p $dir && shift && /bin/mv $* $dir && return
+        [ "$1" = "-f" ] && dir=~/.Trash/`date +%s`  && mkdir -p $dir && shift && /bin/mv $* $dir && return
+        /bin/rm --one-file-system -i $*
+}
+
+Rm -rf /tmp/checkstar
