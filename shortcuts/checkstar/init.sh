3666f1158642156096d3a0e8e123eadb923defeb
Pablo <pablo.aledo@gmail.com>
2023-11-03 03:40:23 +0000

    add init to checkstar

---
+++ shortcuts/checkstar/init.sh
@@ -0,0 +1,5 @@
+[ $# -ge 1 ] && filter=$1 || filter='label: select'
+[ $# -ge 2 ] && file=$2 || file=/tmp/titles
+
+echo $filter > /tmp/checkstar_filter
+[ $file != /tmp/titles ] && \cp $file /tmp/titles
