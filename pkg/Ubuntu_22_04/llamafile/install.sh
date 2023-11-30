834e12f485473ff4f415f337d2ac885f9ad53a99
Pablo <pablo.aledo@gmail.com>
2023-11-30 05:29:16 +0000

    add install.sh for llamafile

---
+++ pkg/Ubuntu_22_04/llamafile/install.sh
@@ -0,0 +1,3 @@
+wget https://huggingface.co/jartine/llava-v1.5-7B-GGUF/resolve/main/llamafile-server-0.1-llava-v1.5-7b-q4
+chmod 755 llamafile-server-0.1-llava-v1.5-7b-q4
+./llamafile-server-0.1-llava-v1.5-7b-q4
