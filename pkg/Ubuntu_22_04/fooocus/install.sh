4bc42c0ddd91de93cf51dfe683f1d2e96f39c7c5
Pablo <pablo.aledo@gmail.com>
2023-12-01 10:51:28 +0000

    add fooocus

---
+++ pkg/Ubuntu_22_04/fooocus/install.sh
@@ -0,0 +1,17 @@
+git clone https://github.com/lllyasviel/Fooocus.git
+cd Fooocus
+conda env create -f environment.yaml
+conda activate fooocus
+pip install -r requirements_versions.txt
+
+python entry_with_update.py
+
+#git clone https://github.com/lllyasviel/Fooocus.git
+#cd Fooocus
+#python3 -m venv fooocus_env
+#source fooocus_env/bin/activate
+#pip install -r requirements_versions.txt
+
+#git clone https://github.com/lllyasviel/Fooocus.git
+#cd Fooocus
+#pip3 install -r requirements_versions.txt
