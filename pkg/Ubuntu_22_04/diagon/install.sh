9d88c764433519bb6854e094f54935a60a98a344
Pablo <pablo.aledo@gmail.com>
2023-12-05 14:47:36 +0000

    add diagon

---
+++ pkg/Ubuntu_22_04/diagon/install.sh
@@ -0,0 +1,5 @@
+cd /tmp
+wget https://github.com/ArthurSonzogni/Diagon/releases/download/v1.1.158/diagon-1.1.156-Linux.tar.gz
+tar -xvzf diagon-1.1.156-Linux.tar.gz
+sudo mv diagon-1.1.156-Linux/bin/diagon-1.1.156 /usr/bin/diagon
+
