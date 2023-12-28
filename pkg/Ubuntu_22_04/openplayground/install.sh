8344945a6f277e8ccf14a7d3b3df62e94f96f03a
Pablo <pablo.aledo@gmail.com>
2023-12-28 13:30:19 +0000

    add openplayground

---
+++ pkg/Ubuntu_22_04/openplayground/install.sh
@@ -0,0 +1,8 @@
+pip install openplayground
+openplayground run
+
+#docker run --name openplayground -p 5432:5432 -d --volume openplayground:/web/config natorg/openplayground
+
+#git clone https://github.com/nat/openplayground
+#cd app && npm install && npx parcel watch src/index.html --no-cache
+#cd server && pip3 install -r requirements.txt && cd .. && python3 -m server.app
