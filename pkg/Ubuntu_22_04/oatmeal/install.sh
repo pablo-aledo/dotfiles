e9d95077faf4af772141777196bf731e23514617
Pablo <pablo.aledo@gmail.com>
2024-01-14 05:35:50 +0000

    add oatmeal

---
+++ pkg/Ubuntu_22_04/oatmeal/install.sh
@@ -0,0 +1,4 @@
+curl -s https://apt.dustinblackman.com/KEY.gpg | apt-key add -
+curl -s https://apt.dustinblackman.com/dustinblackman.list > /etc/apt/sources.list.d/dustinblackman.list
+sudo apt-get update
+sudo apt-get install oatmeal
