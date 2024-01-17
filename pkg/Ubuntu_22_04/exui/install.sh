37406906e806956fbbd066822daca70bf950db4a
Pablo <pablo.aledo@gmail.com>
2024-01-17 05:27:11 +0000

    add exui and koboldcpp

---
+++ pkg/Ubuntu_22_04/exui/install.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/turboderp/exui
+cd exui
+pip install -r requirements.txt
+python server.py
---
+++ pkg/Ubuntu_22_04/koboldcpp/install.sh
@@ -0,0 +1,3 @@
+sudo wget https://github.com/LostRuins/koboldcpp/releases/download/v1.55.1/koboldcpp-linux-x64 -O /usr/bin/koboldcpp
+sudo chmod +x /usr/bin/koboldcpp
+
