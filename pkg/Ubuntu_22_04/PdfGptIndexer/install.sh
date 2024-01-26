4d4bd5f01920075862a26e9894bc032f8908eac2
Pablo <pablo.aledo@gmail.com>
2024-01-26 05:31:18 +0000

    add PdfGptIndexer

---
+++ pkg/Ubuntu_22_04/PdfGptIndexer/install.sh
@@ -0,0 +1,5 @@
+cd
+git clone https://github.com/raghavan/PdfGptIndexer.git
+cd PdfGptIndexer
+pip install langchain openai textract transformers langchain faiss-cpu pypdf tiktoken
+python3 pdf_gpt_indexer.py
