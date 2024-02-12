b229f37aafa8c5a0e432f784979c6fe5b9380328
Pablo <pablo.aledo@gmail.com>
2024-02-12 16:13:32 +0000

    zoom config

--- pkg/Ubuntu_22_04/zoom/install.sh
+++ pkg/Ubuntu_22_04/zoom/install.sh
@@ -1,3 +1,4 @@
 cd /tmp/
 wget https://zoom.us/client/5.11.9.4300/zoom_amd64.deb
 sudo apt install -y ./zoom_amd64.deb
+# sed -i 's/enableMiniWindow=true/enableMiniWindow=false/g' ~/.config/zoomus.conf
