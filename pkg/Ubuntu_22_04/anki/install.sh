ab77d6fedac9af00e8c639d74fd346aef89ff199
Pablo <pablo.aledo@gmail.com>
2024-02-13 05:28:00 +0000

    add anki

---
+++ pkg/Ubuntu_22_04/anki/install.sh
@@ -0,0 +1,6 @@
+cd
+sudo apt install libxcb-xinerama0 libxcb-cursor0 libnss3
+wget https://github.com/ankitects/anki/releases/download/23.12.1/anki-23.12.1-linux-qt6.tar.zst
+tar xaf anki-23.12.1-linux-qt6.tar.zst
+cd anki-23.12.1-linux-qt6
+sudo ./install.sh
