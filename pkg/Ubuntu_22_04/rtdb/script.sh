a6e9eef6e308525211a14cd2c9d002c82d9db5ea
Pablo <pablo.aledo@gmail.com>
2024-03-27 09:06:43 +0000

    fix

--- pkg/Ubuntu_22_04/rtdb/script.sh
+++ pkg/Ubuntu_22_04/rtdb/script.sh
@@ -4,6 +4,6 @@ do
     # get logs
     source innerloop.sh
     sleep 1
-    [ "$(cat graph.im | md5sum)" != "$(cat graph | md5sum)" ] && \cp graph.im graph.final
+    [ "$(cat graph.im | md5sum)" != "$(cat graph.final | md5sum)" ] && \cp graph.im graph.final
 done
 
