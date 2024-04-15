cbd2c4d1cecbeefc901327a31f2226b84496b031
Pablo <pablo.aledo@gmail.com>
2024-04-15 13:48:05 +0000

    notes for blobfuse

--- pkg/Ubuntu_22_04/blobfuse/install.sh
+++ pkg/Ubuntu_22_04/blobfuse/install.sh
@@ -2,3 +2,13 @@ wget https://packages.microsoft.com/config/ubuntu/20.04/packages-microsoft-prod.
 sudo dpkg -i packages-microsoft-prod.deb
 sudo apt-get update
 sudo apt-get install -y blobfuse
+
+#pkg install libfuse2
+
+#cat << EOF > ~/fuse_connection.cfg
+#accountName ...
+#accountKey ...
+#containerName ...
+#EOF
+
+#blobfuse ~/mycontainer --tmp-path=~/mnt/ --config-file=fuse_connection.cfg -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120
