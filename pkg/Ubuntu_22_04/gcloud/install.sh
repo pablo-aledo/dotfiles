c2e45b062ff709ad2c92742279d00adc61ce7599
Pablo <pablo.aledo@gmail.com>
2024-05-10 11:15:09 +0000

    add gcloud installer

---
+++ pkg/Ubuntu_22_04/gcloud/install.sh
@@ -0,0 +1,5 @@
+mkdir ~/gcloud
+cd ~/gcloud
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-472.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-472.0.0-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh
