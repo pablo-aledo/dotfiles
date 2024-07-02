f464fbe87336b4ea228f48a26c2fc4dc083379b1
Pablo <pablo.aledo@gmail.com>
2024-07-02 03:39:37 +0000

    add firefox_selenium

---
+++ pkg/Ubuntu_22_04/firefox_selenium/install.sh
@@ -0,0 +1,3 @@
+wget https://download-installer.cdn.mozilla.net/pub/firefox/releases/116.0.3/linux-x86_64/en-US/firefox-116.0.3.tar.bz2
+tar -xf firefox-116.0.3.tar.bz2 --directory /opt/
+ln -s /opt/firefox/firefox /usr/bin/firefox
