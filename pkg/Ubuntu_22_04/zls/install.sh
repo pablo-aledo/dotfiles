afbe5f466755c04d7d356727eb99ff80b1c0e237
Pablo <pablo.aledo@gmail.com>
2024-07-17 22:19:39 +0000

    add some language servers

--- pkg/Ubuntu_22_04/zls/install.sh
+++ pkg/Ubuntu_22_04/zls/install.sh
@@ -1,1 +1,2 @@
-sudo wget https://zigtools-releases.nyc3.digitaloceanspaces.com/zls/master/x86_64-linux/zls -O /bin/zls
+sudo wget https://zigtools-releases.nyc3.digitaloceanspaces.com/zls/master/x86_64-linux/zls -O /usr/bin/zls
+sudo chmod +x /usr/bin/zls
