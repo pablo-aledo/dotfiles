e9c4886516ea0fe91c56e972e3ea572f2b6d5e53
Pablo <pablo.aledo@gmail.com>
2024-09-08 05:10:39 +0000

    add hn-text

---
+++ pkg/Ubuntu_22_04/hn-text/install.sh
@@ -0,0 +1,5 @@
+cd /tmp/
+wget https://github.com/piqoni/hn-text/releases/download/v0.1.4/hn-text_Linux_x86_64.tar.gz
+tar -xvzf hn-text_Linux_x86_64.tar.gz
+sudo mv hn-text /usr/bin
+sudo chmod +x /usr/bin/hn-text
