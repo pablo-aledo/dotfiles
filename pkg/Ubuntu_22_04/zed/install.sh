ec28b8f2d4982f7ef575811ef48990aaa0345868
Pablo <pablo.aledo@gmail.com>
2024-09-19 04:11:11 +0000

    add zed

--- pkg/Ubuntu_22_04/zed/install.sh
+++ pkg/Ubuntu_22_04/zed/install.sh
@@ -1,1 +1,10 @@
 curl -f https://zed.dev/install.sh | sh
+
+# cd
+# wget https://zed.dev/api/releases/stable/latest/zed-linux-x86_64.tar.gz
+# mkdir -p ~/.local
+# tar -xvf zed-linux-x86_64.tar.gz -C ~/.local
+# ln -sf ~/.local/zed.app/bin/zed ~/.local/bin/zed
+# cp ~/.local/zed.app/share/applications/zed.desktop ~/.local/share/applications/dev.zed.Zed.desktop
+# sed -i "s|Icon=zed|Icon=$HOME/.local/zed.app/share/icons/hicolor/512x512/apps/zed.png|g" ~/.local/share/applications/dev.zed.Zed.desktop
+# sed -i "s|Exec=zed|Exec=$HOME/.local/zed.app/libexec/zed-editor|g" ~/.local/share/applications/dev.zed.Zed.desktop
