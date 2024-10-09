e349b13b502721a2d274acbfc897779adfc0a7d1
Pablo <pablo.aledo@gmail.com>
2024-10-09 10:47:23 +0000

    add helix-gpt

---
+++ pkg/Ubuntu_22_04/helix-gpt/install.sh
@@ -0,0 +1,25 @@
+wget https://github.com/leona/helix-gpt/releases/download/0.34/helix-gpt-0.34-x86_64-linux.tar.gz -O /tmp/helix-gpt.tar.gz \
+&& tar -zxvf /tmp/helix-gpt.tar.gz \
+&& sudo mv helix-gpt-0.34-x86_64-linux /usr/bin/helix-gpt \
+&& sudo chmod +x /usr/bin/helix-gpt
+
+cat <<EOF >> ~/.config/helix/languages.toml
+[language-server.gpt]
+command = "helix-gpt"
+
+[language-server.ts]
+command = "typescript-language-server"
+args = ["--stdio"]
+language-id = "javascript"
+
+[[language]]
+name = "typescript"
+language-servers = [
+    "ts",
+    "gpt"
+]
+EOF
+
+# echo 'export OPENAI_API_KEY="..."' >> ~/.paths
+echo 'export HANDLER=openai' >> ~/.paths
+
