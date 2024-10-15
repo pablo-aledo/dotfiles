f897a7c1bf936c0587465a490badffa8fdb64f8b
Pablo <pablo.aledo@gmail.com>
2024-10-15 04:57:07 +0000

    add blink

---
+++ pkg/Ubuntu_22_04/blink/install.sh
@@ -0,0 +1,5 @@
+cd /tmp
+wget https://github.com/jart/blink/releases/download/1.1.0/blink-1.1.0-linux-x86_64.elf.gz
+gunzip blink-1.1.0-linux-x86_64.elf.gz
+sudo mv blink-1.1.0-linux-x86_64.elf /usr/bin/blink
+sudo chmod +x /usr/bin/blink
