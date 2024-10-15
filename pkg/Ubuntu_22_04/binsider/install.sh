119b948bb92b7a7cdc52afc33f71401d48cfe9d9
Pablo <pablo.aledo@gmail.com>
2024-10-15 02:35:43 +0000

    add binsider

---
+++ pkg/Ubuntu_22_04/binsider/install.sh
@@ -0,0 +1,4 @@
+cd /tmp
+wget https://github.com/orhun/binsider/releases/download/v0.2.1/binsider-0.2.1-x86_64-unknown-linux-gnu.tar.gz
+tar -xvzf binsider-0.2.1-x86_64-unknown-linux-gnu.tar.gz
+sudo mv binsider-0.2.1/binsider /usr/bin
