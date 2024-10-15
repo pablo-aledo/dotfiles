b0a29042f45c5d78db3f28c3d9fb075dbfa3beac
Pablo <pablo.aledo@gmail.com>
2024-10-15 02:07:35 +0000

    add

---
+++ pkg/Ubuntu_22_04/diskonaut/install.sh
@@ -0,0 +1,1 @@
+wget https://github.com/imsnif/diskonaut/releases/download/0.11.0/diskonaut-0.11.0-unknown-linux-musl.tar.gz -O - | sudo tar -xvz -C /usr/bin
---
+++ pkg/Ubuntu_22_04/kbt/install.sh
@@ -0,0 +1,4 @@
+cd /tmp
+wget https://github.com/bloznelis/kbt/releases/download/2.1.0/kbt-2.1.0-x86_64-unknown-linux-gnu.tar.gz
+tar -xvzf kbt-2.1.0-x86_64-unknown-linux-gnu.tar.gz
+sudo mv kbt-2.1.0-x86_64-unknown-linux-gnu/kbt /usr/bin
