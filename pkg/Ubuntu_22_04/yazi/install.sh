a37013c25095c86f5c014592b976a35acd4c0374
Pablo <pablo.aledo@gmail.com>
2024-10-15 02:39:12 +0000

    add yazi

---
+++ pkg/Ubuntu_22_04/yazi/install.sh
@@ -0,0 +1,4 @@
+cd /tmp
+wget https://github.com/sxyazi/yazi/releases/download/v0.3.3/yazi-x86_64-unknown-linux-gnu.zip
+unzip yazi-x86_64-unknown-linux-gnu.zip
+sudo mv yazi-x86_64-unknown-linux-gnu/yazi /usr/bin
