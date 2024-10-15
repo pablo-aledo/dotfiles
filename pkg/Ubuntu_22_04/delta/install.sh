8d354aa1f38efdb5e8d8008db887be4f5021ab98
Pablo <pablo.aledo@gmail.com>
2024-10-15 03:04:26 +0000

    add delta

---
+++ pkg/Ubuntu_22_04/delta/install.sh
@@ -0,0 +1,4 @@
+cd /tmp
+wget https://github.com/dandavison/delta/releases/download/0.18.2/delta-0.18.2-x86_64-unknown-linux-gnu.tar.gz
+tar -xvzf delta-0.18.2-x86_64-unknown-linux-gnu.tar.gz
+sudo mv delta-0.18.2-x86_64-unknown-linux-gnu/delta /usr/bin
