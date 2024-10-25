bc721c7992e6faaaffba7b32f56dd869717cd45c
Pablo <pablo.aledo@gmail.com>
2024-10-25 12:44:30 +0000

    add glow

---
+++ pkg/Ubuntu_22_04/glow/install.sh
@@ -0,0 +1,4 @@
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+sudo apt update && sudo apt install glow
