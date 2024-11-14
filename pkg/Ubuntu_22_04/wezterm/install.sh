368838aaa579656edc1b8b4d539984b2b0843ddc
Pablo <pablo.aledo@gmail.com>
2024-11-14 11:11:42 +0000

    add wezterm

---
+++ pkg/Ubuntu_22_04/wezterm/install.sh
@@ -0,0 +1,3 @@
+curl -LO https://github.com/wez/wezterm/releases/download/20240203-110809-5046fc22/wezterm-20240203-110809-5046fc22.Ubuntu22.04.deb
+sudo apt install -y ./wezterm-20240203-110809-5046fc22.Ubuntu22.04.deb
+
