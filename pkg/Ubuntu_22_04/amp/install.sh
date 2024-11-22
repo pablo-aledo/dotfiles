43c9dd9f2043e15fa7fc406807d8e1e0c6b0e490
Pablo <pablo.aledo@gmail.com>
2024-11-22 04:38:40 +0000

    add amp and ox

---
+++ pkg/Ubuntu_22_04/amp/install.sh
@@ -0,0 +1,2 @@
+sudo wget https://github.com/jmacdonald/amp/releases/download/0.7.1/amp-x86_64-unknown-linux-musl -O /usr/bin/amp
+sudo chmod +x /usr/bin/amp
---
+++ pkg/Ubuntu_22_04/ox/install.sh
@@ -0,0 +1,5 @@
+cd
+wget https://github.com/curlpipe/ox/releases/download/0.7.1/ox_0.7.1-1_amd64.deb
+sudo dpkg -i ox_0.7.1-1_amd64.deb
+#sudo wget https://github.com/curlpipe/ox/releases/download/0.7.1/ox -O /usr/bin/ox
+#sudo chmod +x /usr/bin/ox
