56de6b8ada6feb6fddb0cdb15462f63be26a61ce
Pablo <pablo.aledo@gmail.com>
2024-12-09 05:57:13 +0000

    add rfv to fzf

--- pkg/Ubuntu_22_04/fzf/install.sh
+++ pkg/Ubuntu_22_04/fzf/install.sh
@@ -1,3 +1,4 @@
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 printf 'y\ny\ny\n' | ~/.fzf/install
 sed -i 's/--scheme=history/--tiebreak=index/g' ~/.fzf/shell/key-bindings.zsh
+sudo cp rfv /usr/bin/rfv; sudo chmod +x /usr/bin/rfv
