25334f3a7988c6c800c62299f707ee5720fdbe11
Pablo <pablo.aledo@gmail.com>
2024-12-11 12:41:04 +0000

    add reminder for nvm

--- pkg/Ubuntu_22_04/nodejs/install.sh
+++ pkg/Ubuntu_22_04/nodejs/install.sh
@@ -2,3 +2,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 nvm install 20
 node -v # should print `v20.16.0`
 npm -v # should print `10.8.1`
+#export NVM_DIR="$HOME/.nvm"
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
