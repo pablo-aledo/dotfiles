5a4b3f45912769a8b9e62219ad09b091b5d6abb3
Pablo <pablo.aledo@gmail.com>
2024-12-12 04:39:37 +0000

    update aws cli

--- pkg/Ubuntu_22_04/aws/install.sh
+++ pkg/Ubuntu_22_04/aws/install.sh
@@ -1,7 +1,12 @@
 #sudo apt-get install -y awscli
-sudo snap install aws-cli --classic
+#sudo snap install aws-cli --classic
+
+cd
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 
 echo 'autoload bashcompinit && bashcompinit' >> ~/.paths
 echo 'autoload -Uz compinit && compinit' >> ~/.paths
-echo 'complete -C /usr/bin/aws_completer aws' >> ~/.paths
+echo 'complete -C /usr/local/bin/aws_completer aws' >> ~/.paths
 source ~/.paths
