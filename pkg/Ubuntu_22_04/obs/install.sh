fb63cafb3d98dbde0e3af485f5bae5c41cfc80a9
Pablo <pablo.aledo@gmail.com>
2024-12-18 08:48:47 +0000

    add background removal for obs

--- pkg/Ubuntu_22_04/obs/install.sh
+++ pkg/Ubuntu_22_04/obs/install.sh
@@ -2,3 +2,7 @@ sudo apt update
 sudo add-apt-repository ppa:obsproject/obs-studio
 sudo apt update
 sudo apt install ffmpeg obs-studio
+
+#wget https://github.com/locaal-ai/obs-backgroundremoval/releases/download/1.1.13/obs-backgroundremoval-1.1.13-x86_64-linux-gnu.deb
+#sudo dpkg -i obs-backgroundremoval-1.1.13-x86_64-linux-gnu.deb
+
