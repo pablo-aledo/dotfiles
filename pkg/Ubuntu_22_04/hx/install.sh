34a1737daa60004bb46751f73e495067854ed052
Pablo <pablo.aledo@gmail.com>
2024-12-31 18:00:24 +0000

    hx installers

--- pkg/Ubuntu_22_04/hx/install.sh
+++ pkg/Ubuntu_22_04/hx/install.sh
@@ -2,3 +2,11 @@ sudo add-apt-repository ppa:maveonair/helix-editor
 sudo apt update
 sudo apt install helix
 # hx --health
+
+#cd
+#wget https://github.com/helix-editor/helix/releases/download/24.07/helix-24.07-x86_64-linux.tar.xz
+#unxz helix-24.07-x86_64-linux.tar.xz
+#tar -xvf helix-24.07-x86_64-linux.tar
+#sudo cp helix-24.07-x86_64-linux/hx /usr/bin/hx
+
+#sudo wget https://github.com/helix-editor/helix/releases/download/24.07/helix-24.07-x86_64.AppImage -O /usr/bin/hx
