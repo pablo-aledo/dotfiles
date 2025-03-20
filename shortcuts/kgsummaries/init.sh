88f9569fce38ad28d1c325bedd8f36fcdc89f8ae
Pablo <pablo.aledo@gmail.com>
2025-03-20 16:33:32 +0000

    kgsummaries reminder

--- shortcuts/kgsummaries/init.sh
+++ shortcuts/kgsummaries/init.sh
@@ -1,1 +1,3 @@
 # cat s2link | while read line; do title="$(echo $line | sed 's/\(.*\) -> \(.*\)/\1/g')"; link="$(echo $line | sed 's/\(.*\) -> \(.*\)/\2/g')"; md5=$(echo $title | md5sum | awk '{print $1}'); done | tee /tmp/s2md5
+# [ "$(md5sum $(ltr | tail -n1 | tail -n1 | awk '{print $NF}') | awk '{print $1}')" = "$(md5sum $(ltr | tail -n2 | head -n1 | awk '{print $NF}') | awk '{print $1}')" ]
+# [ "$(ps aux G short G kgsummaries.3)" != "" ]
