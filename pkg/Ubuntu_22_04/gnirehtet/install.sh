38bc6139fd4a0bf13e48f0b018f026e9e6b7a8e0
Pablo <pablo.aledo@gmail.com>
2025-04-22 11:25:30 +0000

    add gnirehtet

---
+++ pkg/Ubuntu_22_04/gnirehtet/install.sh
@@ -0,0 +1,11 @@
+# Opciones de desarrollador
+#   Depuracion USB
+#   Instalar via USB
+#   Depuracion USB (Ajustes de seguridad)
+# Punto de acceso portatil
+#   Compartir internet via USB
+wget https://github.com/Genymobile/gnirehtet/releases/download/v2.5.1/gnirehtet-rust-linux64-v2.5.1.zip
+unzip gnirehtet-rust-linux64-v2.5.1.zip
+cd gnirehtet-rust-linux64
+./gnirehtet install
+./gnirehtet run
