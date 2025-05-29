504afa18fac58595e50e03ba120f0aeba2b3bad3
Pablo <pablo.aledo@gmail.com>
2025-05-29 06:45:21 +0000

    add bluetooth-midi

--- pkg/Ubuntu_22_04/bluetooth-midi/install.sh
+++ pkg/Ubuntu_22_04/bluetooth-midi/install.sh
@@ -1,3 +1,4 @@
+# sudo apt install libudev-dev libical-dev libreadline-dev libdbus-1-dev libasound2-dev build-essential
 cd /tmp
 wget https://mirrors.edge.kernel.org/pub/linux/bluetooth/bluez-5.82.tar.xz
 tar -xf bluez-5.82.tar.xz
