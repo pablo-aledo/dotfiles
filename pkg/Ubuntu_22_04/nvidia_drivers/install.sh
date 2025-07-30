a5ab3e17684b426e5412c8421710b4a491e5ce60
Pablo <pablo.aledo@gmail.com>
2025-07-30 08:19:48 +0000

    add nvidia_drivers

---
+++ pkg/Ubuntu_22_04/nvidia_drivers/install.sh
@@ -0,0 +1,3 @@
+wget https://us.download.nvidia.com/XFree86/Linux-x86_64/570.172.08/NVIDIA-Linux-x86_64-570.172.08.run
+chmod +x NVIDIA-Linux-x86_64-570.172.08.run
+sudo ./NVIDIA-Linux-x86_64-570.172.08.run
