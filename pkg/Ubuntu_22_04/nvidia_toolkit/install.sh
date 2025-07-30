82605a236b3d7efafdb14585a8121e0645aefde0
Pablo <pablo.aledo@gmail.com>
2025-07-30 10:39:04 +0000

    downgrade nvidia-toolkit

--- pkg/Ubuntu_22_04/nvidia_toolkit/install.sh
+++ pkg/Ubuntu_22_04/nvidia_toolkit/install.sh
@@ -5,15 +5,15 @@
 # sudo reboot
 # nvidia-smi
 
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo mv cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-ubuntu2204.pin
+sudo mv cuda-ubuntu2204.pin /etc/apt/preferences.d/cuda-repository-pin-600
 
-wget https://developer.download.nvidia.com/compute/cuda/12.9.1/local_installers/cuda-repo-ubuntu2004-12-9-local_12.9.1-575.57.08-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu2004-12-9-local_12.9.1-575.57.08-1_amd64.deb
-sudo cp /var/cuda-repo-ubuntu2004-12-9-local/cuda-*-keyring.gpg /usr/share/keyrings/
+wget https://developer.download.nvidia.com/compute/cuda/12.8.0/local_installers/cuda-repo-ubuntu2204-12-8-local_12.8.0-570.86.10-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu2204-12-8-local_12.8.0-570.86.10-1_amd64.deb
+sudo cp /var/cuda-repo-ubuntu2204-12-8-local/cuda-*-keyring.gpg /usr/share/keyrings/
 
 sudo apt-get update
-sudo apt-get -y install cuda-toolkit-12-9
+sudo apt-get -y install cuda-toolkit-12-8
 
 echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.paths
 echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.paths
