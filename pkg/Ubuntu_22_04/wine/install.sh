a6248b06e801f087a338f553f758630a63b7fbff
Pablo <pablo.aledo@gmail.com>
2025-07-31 09:49:30 +0000

    add wine

---
+++ pkg/Ubuntu_22_04/wine/install.sh
@@ -0,0 +1,20 @@
+sudo dpkg --add-architecture i386
+$ dpkg --print-foreign-architectures
+
+sudo mkdir -pm755 /etc/apt/keyrings
+wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo gpg --dearmor -o /etc/apt/keyrings/winehq-archive.key -
+
+sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+
+sudo apt update
+
+sudo apt install --install-recommends winehq-stable
+#sudo apt install --install-recommends winehq-devel
+#sudo apt install --install-recommends winehq-staging
+sudo apt install winetricks
+
+#wine --version
+#wine winecfg
+#wine clock
+#wine iexplore
+#msiexec -i installer_name.msi
