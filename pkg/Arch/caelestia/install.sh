d36e28685f8d56d32c85071d5cc134aebe258c24
Pablo <pablo.aledo@gmail.com>
2025-08-02 02:23:27 +0000

    add caelestia

---
+++ pkg/Arch/caelestia/install.sh
@@ -0,0 +1,6 @@
+sudo pacman -S git wget curl gcc make cmake
+git clone https://github.com/caelestia-dots/caelestia.git ~/.local/share/caelestia
+~/.local/share/caelestia/install.fish
+#sudo pacman -S firefox libreoffice-fresh gwenview kate dolphin ark okular flatpak discover discord swayimg
+sudo pacman -S bluez bluez-utils sddm
+sudo systemctl start sddm
