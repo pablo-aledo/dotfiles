690a29ff4153adf9144b765bf30bba8111c9eef2
Pablo <pablo.aledo@gmail.com>
2025-08-09 02:38:59 +0000

    winetkg64

--- pkg/Ubuntu_22_04/wine-tkg/install.sh
+++ pkg/Ubuntu_22_04/wine-tkg/install.sh
@@ -1,14 +1,23 @@
 cd
 git clone https://github.com/Kron4ek/wine-tkg.git
 cd wine-tkg
-sudo dpkg --add-architecture i386
+
+#sudo dpkg --add-architecture i386
 sudo apt update
-sudo apt install -y gcc-multilib g++-multilib libc6-dev-i386
+
+#sudo apt install -y gcc-multilib g++-multilib libc6-dev-i386
+#sudo apt install -y flex bison
+#sudo apt install -y libx11-dev:i386 libxext-dev:i386 libxrandr-dev:i386 libxrender-dev:i386 libxcursor-dev:i386 libxi-dev:i386 libxinerama-dev:i386
+#sudo apt install -y libgl1-mesa-dev:i386 libglu1-mesa-dev:i386
+#sudo apt install -y libfreetype6-dev:i386 pkgconf
+#./configure
+sudo apt install -y gcc g++ libc6-dev
 sudo apt install -y flex bison
-sudo apt install -y libx11-dev:i386 libxext-dev:i386 libxrandr-dev:i386 libxrender-dev:i386 libxcursor-dev:i386 libxi-dev:i386 libxinerama-dev:i386
-sudo apt install -y libgl1-mesa-dev:i386 libglu1-mesa-dev:i386
-sudo apt install -y libfreetype6-dev:i386
+sudo apt install -y libx11-dev libxext-dev libxrandr-dev libxrender-dev libxcursor-dev libxi-dev libxinerama-dev
+sudo apt install -y libgl1-mesa-dev libglu1-mesa-dev
+sudo apt install -y libfreetype6-dev pkgconf
 ./configure
-#./configure --enable-win64
+./configure --enable-win64
+
 make
 #sudo make install
