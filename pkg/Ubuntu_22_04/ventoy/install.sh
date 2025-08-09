34a3920cdf2077f0ab859c7c3c3c96f655988dae
Pablo <pablo.aledo@gmail.com>
2025-08-09 03:17:49 +0000

    ventoy

---
+++ pkg/Ubuntu_22_04/ventoy/install.sh
@@ -0,0 +1,5 @@
+cd
+wget https://github.com/ventoy/Ventoy/releases/download/v1.1.05/ventoy-1.1.05-linux.tar.gz
+tar -xvzf ventoy-1.1.05-linux.tar.gz
+cd ventoy-1.1.05
+# sh Ventoy2Disk.sh { -i | -I | -u } /dev/XXX
