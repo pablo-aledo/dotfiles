3c11d89a2443a925456836a774434e2e7475550f
Pablo <pablo.aledo@gmail.com>
2025-10-04 03:26:25 +0000

    add wine GE to bottles

--- pkg/Ubuntu_22_04/wine_bottles/install.sh
+++ pkg/Ubuntu_22_04/wine_bottles/install.sh
@@ -2,4 +2,8 @@ sudo apt update
 sudo apt install flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install flathub com.usebottles.bottles
+
+cd ~/.var/app/com.usebottles.bottles/data/bottles/runners
+wget https://github.com/GloriousEggroll/proton-ge-custom/releases/download/GE-Proton10-17/GE-Proton10-17.tar.gz -O - | tar -xvz
+
 flatpak run com.usebottles.bottles
