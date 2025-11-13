b29815ae8fd45a910522bb16672b2d586db1fb6d
Pablo <pablo.aledo@gmail.com>
2025-11-13 20:02:46 +0000

    add chordcat

---
+++ pkg/Ubuntu_22_04/chordcat/install.sh
@@ -0,0 +1,4 @@
+sudo apt install flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install flathub dev.ters.Chordcat
+flatpak run dev.ters.Chordcat
