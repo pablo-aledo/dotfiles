c93c9043cb7ac16fc5a5b66212960eda38015d79
Pablo <pablo.aledo@gmail.com>
2025-11-24 05:10:45 +0000

    complete nosuspend

--- pkg/Ubuntu_22_04/nosuspend/install.sh
+++ pkg/Ubuntu_22_04/nosuspend/install.sh
@@ -1,1 +1,7 @@
 sudo sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=ignore/g' /etc/systemd/logind.conf
+sudo systemctl mask sleep.target
+sudo systemctl mask suspend.target
+sudo systemctl mask hibernate.target
+sudo systemctl mask hybrid-sleep.target
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
