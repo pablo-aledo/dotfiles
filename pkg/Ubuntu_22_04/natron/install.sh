c8e71fa34f83b8c9f93d2ca13459baf997364d4d
Pablo <pablo.aledo@gmail.com>
2026-01-09 03:05:56 +0000

    add natron

---
+++ pkg/Ubuntu_22_04/natron/install.sh
@@ -0,0 +1,4 @@
+cd
+wget https://github.com/NatronGitHub/Natron/releases/latest/download/Natron-2.5.0-Linux-x86_64-no-installer.tar.xz
+unxz Natron-2.5.0-Linux-x86_64-no-installer.tar.xz
+tar -xvf Natron-2.5.0-Linux-x86_64-no-installer.tar
