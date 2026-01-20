f175f14678f8045ae5a75e75ec33a4057bc7ecf8
Pablo <pablo.aledo@gmail.com>
2026-01-20 07:11:34 +0000

    add antigravity

---
+++ pkg/Ubuntu_22_04/antigravity/install.sh
@@ -0,0 +1,7 @@
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://us-central1-apt.pkg.dev/doc/repo-signing-key.gpg | \
+  sudo gpg --dearmor --yes -o /etc/apt/keyrings/antigravity-repo-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/antigravity-repo-key.gpg] https://us-central1-apt.pkg.dev/projects/antigravity-auto-updater-dev/ antigravity-debian main" | \
+  sudo tee /etc/apt/sources.list.d/antigravity.list > /dev/null
+sudo apt update
+sudo apt install antigravity
