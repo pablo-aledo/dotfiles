171e282c8a8a4a63c15422a5af914a10c5290878
Pablo <pablo.aledo@gmail.com>
2026-02-23 06:21:15 +0000

    timidity with soundfont and fx

--- pkg/Ubuntu_22_04/timidity/install.sh
+++ pkg/Ubuntu_22_04/timidity/install.sh
@@ -1,3 +1,4 @@
 ./configure --prefix=/usr --with-default-path=/etc/timidity/ --enable-audio=alsa --enable-ncurses
 make
 # alias timidity='timidity -int'
+# alias timidity='timidity -int -EFreverb=2 -EFchorus=2 -x "soundfont ~/Descargas/SGM-V2.01.sf2"'
