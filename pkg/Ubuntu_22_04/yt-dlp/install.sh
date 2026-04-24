5621cc5a517ebd99491e19f746b13b45a7e3d9fd
Pablo <pablo.aledo@gmail.com>
2026-04-24 11:23:12 +0000

    add reminder

--- pkg/Ubuntu_22_04/yt-dlp/install.sh
+++ pkg/Ubuntu_22_04/yt-dlp/install.sh
@@ -1,3 +1,7 @@
 sudo add-apt-repository ppa:tomtomtom/yt-dlp
 sudo apt update
 sudo apt install yt-dlp
+
+#sudo rm -f /usr/bin/yt-dlp
+#sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
+#sudo chmod a+rx /usr/local/bin/yt-dlp
