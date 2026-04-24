efd7345b0330c7be21485bc8353b63ffe5155dfd
Pablo <pablo.aledo@gmail.com>
2026-04-24 13:42:22 +0000

    nev

---
+++ pkg/Ubuntu_22_04/nev/install.sh
@@ -0,0 +1,3 @@
+wget https://github.com/Nimaoth/Nev/releases/download/v0.5.0/nev-0.5.0-x86_64-unknown-linux-gnu.tar -O - | tar -xj -C /opt/
+sudo chmod +x /opt/nev-0.5.0-x86_64-unknown-linux-gnu/nev
+sudo ln -s /opt/nev-0.5.0-x86_64-unknown-linux-gnu/nev /usr/local/bin
