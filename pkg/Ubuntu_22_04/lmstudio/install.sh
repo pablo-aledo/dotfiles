a3efa77cf396ea74054505c1e2851c7f4b06af94
Pablo <pablo.aledo@gmail.com>
2026-05-04 13:09:07 +0000

    update

--- pkg/Ubuntu_22_04/lmstudio/install.sh
+++ pkg/Ubuntu_22_04/lmstudio/install.sh
@@ -1,2 +1,4 @@
 sudo wget https://s3.amazonaws.com/releases.lmstudio.ai/prerelease/LM+Studio-0.2.8-beta-v1.AppImage -O /usr/bin/lmstudio
 sudo chmod +x /usr/bin/lmstudio
+
+# curl -fsSL https://lmstudio.ai/install.sh | bash
