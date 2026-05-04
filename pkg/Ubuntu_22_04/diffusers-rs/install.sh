612c77e305b693c4dad4958dbbddff8af4d65297
Pablo <pablo.aledo@gmail.com>
2026-05-04 20:04:01 +0000

    rust dl libraries

---
+++ pkg/Ubuntu_22_04/diffusers-rs/install.sh
@@ -0,0 +1,1 @@
+cargo run --example stable-diffusion --features clap -- --prompt "A rusty robot holding a fire torch."
---
+++ pkg/Ubuntu_22_04/minitensor/install.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/neuralsorcerer/minitensor.git
+cd minitensor
+make install
