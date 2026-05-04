de5712cf05e4ab3d97281840f757ef678fd9f9cf
Pablo <pablo.aledo@gmail.com>
2026-05-04 13:07:12 +0000

    add zrythm

---
+++ pkg/Ubuntu_22_04/zrythm/install.sh
@@ -0,0 +1,4 @@
+wget https://github.com/zrythm/zrythm/releases/download/v1.0.0/zrythm-trial-1.0.0-installer.zip
+unzip zrythm-trial-1.0.0-installer.zip
+cd zrythm-trial-1.0.0-installer
+./install.sh
