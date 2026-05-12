59997b484a7fb01355380a4cf04e03617ca57ebd
Pablo <pablo.aledo@gmail.com>
2026-05-12 09:37:13 +0000

    add kitty

---
+++ pkg/Ubuntu_22_04/kitty/install.sh
@@ -0,0 +1,5 @@
+curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+#GTK_IM_MODULE=xim kitty
+#echo 'linux_display_server x11'   >> ~/.config/kitty/kitty.conf
+#echo 'kitty_keyboard_protocol no' >> ~/.config/kitty/kitty.conf
+#TERM=xterm-256color ssh servidor
