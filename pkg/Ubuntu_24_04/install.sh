f61c7779637ec032fd1f89dd3fa2b90690ad8c73
Pablo <pablo.aledo@gmail.com>
2026-05-27 12:43:37 +0000

    docker

---
+++ pkg/Ubuntu_24_04/install.sh
@@ -0,0 +1,28 @@
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install -y ca-certificates curl gnupg
+
+sudo install -m 0755 -d /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+echo \
+"deb [arch=$(dpkg --print-architecture) \
+signed-by=/etc/apt/keyrings/docker.gpg] \
+https://download.docker.com/linux/ubuntu \
+$(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+
+sudo apt install -y docker-ce docker-ce-cli containerd.io \
+docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker $USER
+
+sudo systemctl enable docker
+sudo systemctl start docker
