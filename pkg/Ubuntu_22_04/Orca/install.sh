a7c005eec838a949be0fc9b97f08938874718337
Pablo <pablo.aledo@gmail.com>
2026-05-27 05:20:30 +0000

    Orca-c

--- pkg/Ubuntu_22_04/Orca/install.sh
+++ pkg/Ubuntu_22_04/Orca/install.sh
@@ -2,3 +2,9 @@ git clone https://github.com/hundredrabbits/Orca.git
 cd Orca/desktop/
 npm install
 npm start
+
+#sudo apt-get install git libncurses5-dev libncursesw5-dev libportmidi-dev
+#git clone https://github.com/hundredrabbits/Orca-c.git
+#cd Orca-c
+#make          # Compile orca
+#build/orca    # Run orca
