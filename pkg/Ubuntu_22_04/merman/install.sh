966d24a34e9f823f41dc8da847b9605609c7896f
Pablo <pablo.aledo@gmail.com>
2026-06-11 06:24:05 +0000

    add merman

---
+++ pkg/Ubuntu_22_04/merman/install.sh
@@ -0,0 +1,4 @@
+cd /tmp
+wget https://github.com/Latias94/merman/releases/download/v0.8.0-alpha.1/merman-cli-x86_64-unknown-linux-gnu.tar.xz
+tar -xvJf merman-cli-x86_64-unknown-linux-gnu.tar.xz
+sudo cp merman-cli-x86_64-unknown-linux-gnu/merman-cli /usr/bin
