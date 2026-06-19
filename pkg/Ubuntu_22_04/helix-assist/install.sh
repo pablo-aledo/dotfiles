ef13c187a10e91e2412806c398124dc3296e4cd8
Pablo <pablo.aledo@gmail.com>
2026-06-19 15:04:36 +0000

    add helix-assist

---
+++ pkg/Ubuntu_22_04/helix-assist/install.sh
@@ -0,0 +1,3 @@
+wget https://github.com/leona/helix-assist/releases/latest/download/helix-assist-linux-amd64
+chmod +x helix-assist-linux-amd64
+sudo mv helix-assist-linux-amd64 /usr/local/bin/helix-assist
