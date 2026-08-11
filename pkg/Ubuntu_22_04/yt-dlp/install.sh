a2ced2fefa27c3090d09ab2bdde3402809d586fc
Pablo <pablo.aledo@gmail.com>
2026-08-11 06:50:19 +0000

    yt-dlp deno

--- pkg/Ubuntu_22_04/yt-dlp/install.sh
+++ pkg/Ubuntu_22_04/yt-dlp/install.sh
@@ -5,3 +5,6 @@ sudo apt install yt-dlp
 #sudo rm -f /usr/bin/yt-dlp
 #sudo curl -L https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp -o /usr/local/bin/yt-dlp
 #sudo chmod a+rx /usr/local/bin/yt-dlp
+
+#curl -fsSL https://deno.land/install.sh | sh
+#export PATH="$HOME/.deno/bin:$PATH"
